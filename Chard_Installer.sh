b6ac4554d3d12746ab44ef227a5a7a0badd1b69c
shadowed1 <itszachg@gmail.com>
2025-10-25 19:02:50 -0400

    Update Chard_Installer.sh

--- Chard_Installer.sh
+++ Chard_Installer.sh
@@ -1530,6 +1530,7 @@ sudo chroot "$CHARD_ROOT" /bin/bash -c "
                 CHARD_USER=\$(cat /.chard_user)
                 HOME=\$CHARD_HOME
                 USER=\$CHARD_USER
+                /bin/SMRT
                 source \$HOME/.bashrc 2>/dev/null
                 env-update
                 dbus-daemon --system --fork 2>/dev/null
