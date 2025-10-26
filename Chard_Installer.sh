9f905d3040b80494388c1dcf47eec8b2f38f4093
shadowed1 <itszachg@gmail.com>
2025-10-26 05:30:30 -0400

    Update Chard_Installer.sh

--- Chard_Installer.sh
+++ Chard_Installer.sh
@@ -1173,7 +1173,7 @@ sudo chroot "$CHARD_ROOT" /bin/bash -c "
     CHARD_USER=\$(cat /.chard_user)
     HOME=\$CHARD_HOME
     USER=\$CHARD_USER
-    
+    su \$USER
     chown \$USER:\$USER /var/lib/portage/world
     chmod 644 /var/lib/portage/world 
     source \$HOME/.bashrc 2>/dev/null
