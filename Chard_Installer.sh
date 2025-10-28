b9c6139694ae72c096dd4ef0e0ef2acbf0632e2e
shadowed1 <itszachg@gmail.com>
2025-10-28 10:52:20 -0400

    Update Chard_Installer.sh

--- Chard_Installer.sh
+++ Chard_Installer.sh
@@ -353,21 +353,53 @@ sudo chroot "$CHARD_ROOT" /bin/bash -c "
 CHARD_USER=\$(cat /.chard_user)
 CHARD_HOME=\$(cat /.chard_home)
 
-GROUP_ID=1000
-USER_ID=1000
-
-getent group \$GROUP_ID >/dev/null || groupadd -g \$GROUP_ID wayland
+getent group 1000 >/dev/null   || groupadd -g 1000 chronos
+getent group 601 >/dev/null    || groupadd -g 601 wayland
+getent group 602 >/dev/null    || groupadd -g 602 arc-bridge
+getent group 20205 >/dev/null  || groupadd -g 20205 arc-keymintd
+getent group 604 >/dev/null    || groupadd -g 604 arc-sensor
+getent group 665357 >/dev/null || groupadd -g 665357 android-everybody
+getent group 18 >/dev/null     || groupadd -g 18 audio
+getent group 222 >/dev/null    || groupadd -g 222 input
+getent group 7 >/dev/null      || groupadd -g 7 lp
+getent group 27 >/dev/null     || groupadd -g 27 video
+getent group 423 >/dev/null    || groupadd -g 423 bluetooth-audio
+getent group 600 >/dev/null    || groupadd -g 600 cras
+getent group 85 >/dev/null     || groupadd -g 85 usb
+getent group 20162 >/dev/null  || groupadd -g 20162 traced-producer
+getent group 20164 >/dev/null  || groupadd -g 20164 traced-consumer
+getent group 1001 >/dev/null   || groupadd -g 1001 chronos-access
+getent group 240 >/dev/null    || groupadd -g 240 brltty
+getent group 20150 >/dev/null  || groupadd -g 20150 arcvm-boot-notification-server
+getent group 20189 >/dev/null  || groupadd -g 20189 arc-mojo-proxy
+getent group 20152 >/dev/null  || groupadd -g 20152 arc-host-clock
+getent group 608 >/dev/null    || groupadd -g 608 midis
+getent group 415 >/dev/null    || groupadd -g 415 suzy-q
+getent group 612 >/dev/null    || groupadd -g 612 ml-core
+getent group 311 >/dev/null    || groupadd -g 311 fuse-archivemount
+getent group 20137 >/dev/null  || groupadd -g 20137 crash
+getent group 419 >/dev/null    || groupadd -g 419 crash-access
+getent group 420 >/dev/null    || groupadd -g 420 crash-user-access
+getent group 304 >/dev/null    || groupadd -g 304 fuse-drivefs
+getent group 20215 >/dev/null  || groupadd -g 20215 regmond_senders
+getent group 603 >/dev/null    || groupadd -g 603 arc-camera
+getent group 20042 >/dev/null  || groupadd -g 20042 camera
+getent group 208 >/dev/null    || groupadd -g 208 pkcs11
+getent group 303 >/dev/null    || groupadd -g 303 policy-readers
+getent group 20132 >/dev/null  || groupadd -g 20132 arc-keymasterd
+getent group 605 >/dev/null    || groupadd -g 605 debugfs-access
 
 if ! id \"\$CHARD_USER\" &>/dev/null; then
-    useradd -u \$USER_ID -g \$GROUP_ID -d \"/\$CHARD_HOME\" -M -s /bin/bash \"\$CHARD_USER\"
+    useradd -u 1000 -g 1000 -d \"/\$CHARD_HOME\" -M -s /bin/bash \"\$CHARD_USER\"
 fi
 
+usermod -aG chronos,wayland,arc-bridge,arc-keymintd,arc-sensor,android-everybody,audio,input,lp,video,bluetooth-audio,cras,usb,traced-producer,traced-consumer,chronos-access,brltty,arcvm-boot-notification-server,arc-mojo-proxy,arc-host-clock,midis,suzy-q,ml-core,fuse-archivemount,crash,crash-access,crash-user-access,fuse-drivefs,regmond_senders,arc-camera,camera,pkcs11,policy-readers,arc-keymasterd,debugfs-access \$CHARD_USER
+
 mkdir -p \"/\$CHARD_HOME\"
-chown \$USER_ID:\$GROUP_ID \"/\$CHARD_HOME\"
+chown 1000:1000 \"/\$CHARD_HOME\"
 
 mkdir -p /etc/sudoers.d
 chmod 750 /etc/sudoers.d
-
 echo \"\$CHARD_USER ALL=(ALL) NOPASSWD: ALL\" > /etc/sudoers.d/\$CHARD_USER
 chmod 440 /etc/sudoers.d/\$CHARD_USER
 
