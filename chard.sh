5931da99df55438033b9b4cd7bbf005262d04c5e
shadowed1 <itszachg@gmail.com>
2025-10-29 19:22:22 -0400

    Update chard.sh

--- chard.sh
+++ chard.sh
@@ -582,6 +582,7 @@ EOF
             sudo umount -l "$CHARD_ROOT/run/cras"     2>/dev/null || true
             sudo umount -l "$CHARD_ROOT/dev/input"    2>/dev/null || true
             sudo umount -l "$CHARD_ROOT/dev/dri"      2>/dev/null || true
+            sudo umount -l "$CHARD_ROOT/dev"          2>/dev/null || true
             sudo umount -l "$CHARD_ROOT/run/dbus"     2>/dev/null || true
             sudo umount -l "$CHARD_ROOT/run/chrome"   2>/dev/null || true
             echo "${BLUE}[*] Removing $CHARD_ROOT${RESET}"
