ba93b8ebf5de477e37463e1e6d693944c233e9dd
shadowed1 <itszachg@gmail.com>
2025-10-29 19:23:11 -0400

    Update Chard_Installer.sh

--- Chard_Installer.sh
+++ Chard_Installer.sh
@@ -148,6 +148,7 @@ sudo umount -l "$CHARD_ROOT/proc"         2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/run/cras"     2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/dev/input"    2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/dev/dri"      2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/dev"          2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/run/dbus"     2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/run/chrome"   2>/dev/null || true
 echo "${RED}[*] Removing $CHARD_ROOT...${RESET}"
@@ -1778,6 +1779,7 @@ sudo umount -l "$CHARD_ROOT/proc"         2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/run/cras"     2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/dev/input"    2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/dev/dri"      2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/dev"          2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/run/dbus"     2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/run/chrome"   2>/dev/null || true
 show_progress
