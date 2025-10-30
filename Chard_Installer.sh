f6d5bac57b7cb509fc5299f2a5cd04ab2aae6e7c
shadowed1 <itszachg@gmail.com>
2025-10-29 21:12:36 -0400

    Update Chard_Installer.sh

--- Chard_Installer.sh
+++ Chard_Installer.sh
@@ -142,7 +142,6 @@ echo "${RESET}${RED}[*] Unmounting active bind mounts...${RESET}"
 sudo umount -l "$CHARD_ROOT/etc/ssl"      2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/dev/pts"      2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/dev/shm"      2>/dev/null || true
-sudo umount -l "$CHARD_ROOT/dev"          2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/sys"          2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/proc"         2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/run/cras"     2>/dev/null || true
