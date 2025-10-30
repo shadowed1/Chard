f3eb987e93fe3b5b8136da4b19d490863927c11a
shadowed1 <itszachg@gmail.com>
2025-10-29 21:30:44 -0400

    Update Chard_Installer.sh

--- Chard_Installer.sh
+++ Chard_Installer.sh
@@ -139,17 +139,17 @@ echo
 echo "${GREEN}[+] Creating ${RESET}${RED}Chard Root${RESET}"
 
 echo "${RESET}${RED}[*] Unmounting active bind mounts...${RESET}"
-sudo umount -l "$CHARD_ROOT/etc/ssl"      2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/dev/pts"      2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/dev/shm"      2>/dev/null || true
-sudo umount -l "$CHARD_ROOT/sys"          2>/dev/null || true
-sudo umount -l "$CHARD_ROOT/proc"         2>/dev/null || true
-sudo umount -l "$CHARD_ROOT/run/cras"     2>/dev/null || true
-sudo umount -l "$CHARD_ROOT/dev/input"    2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/dev/dri"      2>/dev/null || true
-sudo umount -l "$CHARD_ROOT/dev"          2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/dev/input"    2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/run/cras"     2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/run/dbus"     2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/run/chrome"   2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/dev"          2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/sys"          2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/proc"         2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/etc/ssl"      2>/dev/null || true
 echo "${RED}[*] Removing $CHARD_ROOT...${RESET}"
 sudo rm -rf "$CHARD_ROOT"
 CURRENT_SHELL=$(basename "$SHELL")
