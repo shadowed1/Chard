6c6ffd17f4adc1d079002d737991a22a58f3ed57
shadowed1 <itszachg@gmail.com>
2025-10-31 13:47:50 -0400

    Update Uninstall_Chard.sh

--- Uninstall_Chard.sh
+++ Uninstall_Chard.sh
@@ -1,3 +1,11 @@
+# Chard Uninstaller
+
+# <<< CHARD_ROOT_MARKER >>>
+CHARD_ROOT=""
+CHARD_HOME=""
+CHARD_USER=""
+# <<< END_CHARD_ROOT_MARKER >>>
+
 echo "${RESET}${RED}[*] Unmounting active bind mounts...${RESET}"
 sudo umount -l "$CHARD_ROOT/run/cras"   2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/dev/input"  2>/dev/null || true
@@ -12,3 +20,4 @@ sudo umount -l "$CHARD_ROOT/sys"        2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/proc"       2>/dev/null || true
 echo "${RED}[*] Removing $CHARD_ROOT...${RESET}"
 sudo rm -rf "$CHARD_ROOT"
+echo "${GREEN}Chard is uninstalled!${RESET}
