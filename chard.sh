f034b34d5ecebf76a12995473c6085ba66bb228d
shadowed1 <itszachg@gmail.com>
2025-11-02 04:01:13 -0500

    Update chard.sh

--- chard.sh
+++ chard.sh
@@ -353,7 +353,7 @@ case "$cmd" in
         sudo umount -l "$CHARD_ROOT/run/dbus"   2>/dev/null || true
         sudo umount -l "$CHARD_ROOT/run/chrome" 2>/dev/null || true
         sudo umount -l "$CHARD_ROOT/run/user/1000" 2>/dev/null || true
-        sudo umount -l "$CHARD_ROOT/$CHARD_HOME/bwrap" 2>/dev/null || true
+        sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap" 2>/dev/null || true
         ;;
     chariot)
         sudo mountpoint -q "$CHARD_ROOT/run/user/1000" || sudo mount --bind /run/user/1000 "$CHARD_ROOT/run/user/1000" 2>/dev/null
