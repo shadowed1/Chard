1f4ca6ea38db61fee778ad5fbd64131c44c7e8cb
shadowed1 <itszachg@gmail.com>
2025-11-01 23:18:43 -0400

    Update chard.sh

--- chard.sh
+++ chard.sh
@@ -333,8 +333,8 @@ case "$cmd" in
                 source \$HOME/.bashrc 2>/dev/null
                 source \$HOME/.smrt_env.sh 2>/dev/null
                 dbus-daemon --system --fork 2>/dev/null
-                chard_sommelier && exec /bin/bash
                 trap exit 1 INT TERM
+                chard_sommelier && exec /bin/bash
             "
             
             umount -l /dev/zram0   2>/dev/null || true
