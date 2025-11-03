519eb62bb98352c36fc88324a74e64bd9b8c204b
shadowed1 <itszachg@gmail.com>
2025-11-02 21:04:58 -0500

    Update chard.sh

--- chard.sh
+++ chard.sh
@@ -290,15 +290,18 @@ case "$cmd" in
          chard_uninstall
         ;;
     root)
-        sudo mount --bind "$CHARD_ROOT" "$CHARD_ROOT"
-        sudo mount --bind "$CHARD_ROOT/$CHARD_HOME" "$CHARD_ROOT/$CHARD_HOME"
-        sudo mount -o remount,exec,symfollow,suid "$CHARD_ROOT/$CHARD_HOME"
-        sudo cp -a "$CHARD_ROOT/usr/bin/bwrap" "$CHARD_ROOT/$CHARD_HOME/" 2>/dev/null
-        sudo mount --bind "$CHARD_ROOT/$CHARD_HOME/bwrap" "$CHARD_ROOT/usr/bin/bwrap" 2>/dev/null
-        sudo chown root:root "$CHARD_ROOT/usr/bin/bwrap" 2>/dev/null
-        sudo chmod u+s "$CHARD_ROOT/usr/bin/bwrap" 2>/dev/null
-        sudo mountpoint -q "$CHARD_ROOT/run/user/1000" || sudo mount --bind /run/user/1000 "$CHARD_ROOT/run/user/1000" 2>/dev/null
-        sudo mountpoint -q "$CHARD_ROOT/run/chrome" || sudo mount --bind /run/chrome "$CHARD_ROOT/run/chrome" 2>/dev/null
+        #sudo mount --bind "$CHARD_ROOT" "$CHARD_ROOT"
+        #sudo mount --bind "$CHARD_ROOT/$CHARD_HOME" "$CHARD_ROOT/$CHARD_HOME"
+        #sudo mount -o remount,exec,symfollow,suid "$CHARD_ROOT/$CHARD_HOME"
+        #sudo cp -a "$CHARD_ROOT/usr/bin/bwrap" "$CHARD_ROOT/$CHARD_HOME/" 2>/dev/null
+        #sudo mount --bind "$CHARD_ROOT/$CHARD_HOME/bwrap" "$CHARD_ROOT/usr/bin/bwrap" 2>/dev/null
+        #sudo chown root:root "$CHARD_ROOT/usr/bin/bwrap" 2>/dev/null
+        #sudo chmod u+s "$CHARD_ROOT/usr/bin/bwrap" 2>/dev/null
+        if [ -f "/home/chronos/user/.bashrc" ]; then
+            sudo mountpoint -q "$CHARD_ROOT/run/chrome" || sudo mount --bind /run/chrome "$CHARD_ROOT/run/chrome" 2>/dev/null
+        else
+            sudo mountpoint -q "$CHARD_ROOT/run/user/1000" || sudo mount --bind /run/user/1000 "$CHARD_ROOT/run/user/1000" 2>/dev/null
+        fi
         sudo mountpoint -q "$CHARD_ROOT/run/dbus"   || sudo mount --bind /run/dbus "$CHARD_ROOT/run/dbus" 2>/dev/null
         sudo mountpoint -q "$CHARD_ROOT/dev/dri"    || sudo mount --bind /dev/dri "$CHARD_ROOT/dev/dri" 2>/dev/null
         sudo mountpoint -q "$CHARD_ROOT/dev/input"  || sudo mount --bind /dev/input "$CHARD_ROOT/dev/input" 2>/dev/null
@@ -355,11 +358,14 @@ case "$cmd" in
         sudo umount -l "$CHARD_ROOT/dev/input"  2>/dev/null || true
         sudo umount -l "$CHARD_ROOT/dev/dri"    2>/dev/null || true
         sudo umount -l "$CHARD_ROOT/run/dbus"   2>/dev/null || true
-        sudo umount -l "$CHARD_ROOT/run/chrome" 2>/dev/null || true
-        sudo umount -l "$CHARD_ROOT/run/user/1000" 2>/dev/null || true
-        sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap" 2>/dev/null || true
-        sudo umount -l "$CHARD_ROOT/$CHARD_HOME" 2>/dev/null || true
-        sudo umount -l "$CHARD_ROOT"
+        if [ -f "/home/chronos/user/.bashrc" ]; then
+            sudo umount -l "$CHARD_ROOT/run/chrome" 2>/dev/null || true
+        else
+            sudo umount -l "$CHARD_ROOT/run/user/1000" 2>/dev/null || true
+        fi
+        #sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap" 2>/dev/null || true
+        #sudo umount -l "$CHARD_ROOT/$CHARD_HOME" 2>/dev/null || true
+        #sudo umount -l "$CHARD_ROOT"
         ;;
     chariot)
         sudo mountpoint -q "$CHARD_ROOT/run/user/1000" || sudo mount --bind /run/user/1000 "$CHARD_ROOT/run/user/1000" 2>/dev/null
