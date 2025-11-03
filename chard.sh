497b415b0bdfca72359d7fff685fcdd3d3d21778
shadowed1 <itszachg@gmail.com>
2025-11-02 21:08:10 -0500

    Update chard.sh

--- chard.sh
+++ chard.sh
@@ -368,8 +368,11 @@ case "$cmd" in
         #sudo umount -l "$CHARD_ROOT"
         ;;
     chariot)
-        sudo mountpoint -q "$CHARD_ROOT/run/user/1000" || sudo mount --bind /run/user/1000 "$CHARD_ROOT/run/user/1000" 2>/dev/null
-        sudo mountpoint -q "$CHARD_ROOT/run/chrome" || sudo mount --bind /run/chrome "$CHARD_ROOT/run/chrome" 2>/dev/null
+        if [ -f "/home/chronos/user/.bashrc" ]; then
+            sudo mountpoint -q "$CHARD_ROOT/run/chrome" || sudo mount --bind /run/chrome "$CHARD_ROOT/run/chrome" 2>/dev/null
+        else
+            sudo mountpoint -q "$CHARD_ROOT/run/user/1000" || sudo mount --bind /run/user/1000 "$CHARD_ROOT/run/user/1000" 2>/dev/null
+        fi
         sudo mountpoint -q "$CHARD_ROOT/run/dbus"   || sudo mount --bind /run/dbus "$CHARD_ROOT/run/dbus" 2>/dev/null
         sudo mountpoint -q "$CHARD_ROOT/dev/dri"    || sudo mount --bind /dev/dri "$CHARD_ROOT/dev/dri" 2>/dev/null
         sudo mountpoint -q "$CHARD_ROOT/dev/input"  || sudo mount --bind /dev/input "$CHARD_ROOT/dev/input" 2>/dev/null
@@ -423,8 +426,11 @@ case "$cmd" in
         sudo umount -l "$CHARD_ROOT/dev/input"  2>/dev/null || true
         sudo umount -l "$CHARD_ROOT/dev/dri"    2>/dev/null || true
         sudo umount -l "$CHARD_ROOT/run/dbus"   2>/dev/null || true
-        sudo umount -l "$CHARD_ROOT/run/chrome" 2>/dev/null || true
-        sudo umount -l "$CHARD_ROOT/run/user/1000" 2>/dev/null || true
+        if [ -f "/home/chronos/user/.bashrc" ]; then
+            sudo umount -l "$CHARD_ROOT/run/chrome" 2>/dev/null || true
+        else
+            sudo umount -l "$CHARD_ROOT/run/user/1000" 2>/dev/null || true
+        fi
         ;;
     categories|cat)
         PORTAGE_DIR="$CHARD_ROOT/usr/portage"
