26a3c980802eeacb226178f862cf0c35c9467fa9
shadowed1 <itszachg@gmail.com>
2025-11-03 19:12:48 -0500

    enhanced?

--- chard_sommelier.sh
+++ chard_sommelier.sh
@@ -1,16 +1,32 @@
 # <<< CHARD .SOMMELIER >>>
 #!/bin/bash
+set -e
+
 export LC_ALL=C
 export SOMMELIER_DISPLAY="/run/chrome/wayland-0"
 export SOMMELIER_DRM_DEVICE="/dev/dri/renderD128"
-export LIBGL_DRIVERS_PATH="$ROOT/usr/lib64/dri"
-export LIBEGL_DRIVERS_PATH="$ROOT/usr/lib64/dri"
+export LIBGL_DRIVERS_PATH="/usr/lib64/dri"
+export LIBEGL_DRIVERS_PATH="/usr/lib64/dri"
 export LD_LIBRARY_PATH=/usr/lib64\${LD_LIBRARY_PATH:+:\$LD_LIBRARY_PATH}
 export LIBGL_ALWAYS_INDIRECT=0
-export QT_QPA_PLATFORM=wayland
 export SOMMELIER_DRM_DEVICE=/dev/dri/renderD128
 export SOMMELIER_GLAMOR=1
 export SOMMELIER_VERSION=0.20
+
+if [ -S /run/chrome/wayland-0 ]; then
+    export WAYLAND_DISPLAY="${WAYLAND_DISPLAY:-/run/chrome/wayland-0}"
+    export WAYLAND_DISPLAY_LOW_DENSITY=${WAYLAND_DISPLAY:-wayland-1}
+    export XDG_SESSION_TYPE=wayland
+    export QT_QPA_PLATFORM=wayland
+    export GDK_BACKEND=wayland
+    export CLUTTER_BACKEND=wayland
+    export EGL_PLATFORM=wayland
+else
+    export XDG_SESSION_TYPE=x11
+    export QT_QPA_PLATFORM=xcb
+    export DISPLAY="${DISPLAY:-:0}"
+    export EGL_PLATFORM=x11
+fi
 
 SOMMELIER_CMD=(
     sommelier
@@ -24,7 +40,7 @@ SOMMELIER_CMD=(
 )
 
 "${SOMMELIER_CMD[@]}" -- bash -c '
-    sleep 0.1
+    sleep 0.2
     export DISPLAY=$(ls /tmp/.X11-unix | sed "s/^X/:/" | head -n1)
     pulseaudio &>/dev/null &
     [ -f ~/.bashrc ] && source ~/.bashrc
