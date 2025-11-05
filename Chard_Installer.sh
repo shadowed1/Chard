fb0c16fbffa813c82502e8e8fce56631fb775cf7
shadowed1 <itszachg@gmail.com>
2025-11-04 22:20:54 -0500

    Update Chard_Installer.sh

--- Chard_Installer.sh
+++ Chard_Installer.sh
@@ -507,9 +507,9 @@ TARGET_FILE=""
 
 if [ -f "$CHROMEOS_BASHRC" ]; then
     TARGET_FILE="$CHROMEOS_BASHRC"
+    sudo mkdir -p "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads"
 else
     TARGET_FILE="$DEFAULT_BASHRC"
-    [ -f "$TARGET_FILE" ] || touch "$TARGET_FILE"
 fi
 
 add_chard_marker() {
