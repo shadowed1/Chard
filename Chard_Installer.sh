08606f39591626ec29189588159812f40c155874
shadowed1 <itszachg@gmail.com>
2025-11-05 23:20:16 -0500

    Update Chard_Installer.sh

--- Chard_Installer.sh
+++ Chard_Installer.sh
@@ -1844,7 +1844,7 @@ else
     sudo mountpoint -q "$CHARD_ROOT/run/cras" || sudo mount --bind /run/user/1000/pulse "$CHARD_ROOT/run/cras" 2>/dev/null
 fi
 
-sudo ln -s $CHARD_ROOT/usr/lib/python-exec /usr/lib/python-exec
+sudo ln -s $CHARD_ROOT/usr/lib/python-exec /usr/lib/python-exec 2>/dev/null
 
 sudo chroot "$CHARD_ROOT" /bin/bash -c '
     mountpoint -q /proc       || mount -t proc proc /proc 2>/dev/null
