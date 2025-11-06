ca2088c448d53e53ca0f2bbe0c7203a0f4893980
shadowed1 <itszachg@gmail.com>
2025-11-06 00:24:24 -0500

    Update Chard_Installer.sh

--- Chard_Installer.sh
+++ Chard_Installer.sh
@@ -1881,7 +1881,7 @@ sudo chroot "$CHARD_ROOT" /bin/bash -c '
 
         dbus-daemon --system --fork 2>/dev/null
         source \$HOME/.bashrc 2>/dev/null
-        source \$HOME/.smrt_env.sh
+        source \$HOME/.smrt_env.sh 2>/dev/null
         sudo chown -R 1000:1000 $HOME
         sudo -E /bin/chariot
     "
