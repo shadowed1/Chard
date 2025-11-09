438f974d6241f0b8a85df11c461d32ea07d5b3f9
shadowed1 <itszachg@gmail.com>
2025-11-09 03:35:45 -0500

    Update Chard_Installer.sh

--- Chard_Installer.sh
+++ Chard_Installer.sh
@@ -163,7 +163,6 @@ cleanup_chroot() {
     sudo umount -l "$CHARD_ROOT/sys"        2>/dev/null || true
     sudo umount -l "$CHARD_ROOT/proc"       2>/dev/null || true
     sudo umount -l "$CHARD_ROOT/tmp/usb_mount" 2>/dev/null || true
-
     sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
     sudo umount -l "$CHARD_ROOT/run/user/1000" 2>/dev/null || true
     sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap" 2>/dev/null || true
