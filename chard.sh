1a522ce2aa0f36730090c4f08884d75cbf36f474
shadowed1 <itszachg@gmail.com>
2025-11-13 16:37:55 -0500

    Update chard.sh

--- chard.sh
+++ chard.sh
@@ -342,20 +342,23 @@ case "$cmd" in
             sudo mountpoint -q "$CHARD_ROOT/run/chrome" || sudo mount --bind /run/chrome "$CHARD_ROOT/run/chrome" 2>/dev/null
             sudo mountpoint -q "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" || sudo mount --bind "/home/chronos/user/MyFiles/Downloads" "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null
             sudo mount -o remount,rw,bind "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads"
-
+        
         else
             sudo mountpoint -q "$CHARD_ROOT/run/user/1000" || sudo mount --bind /run/user/1000 "$CHARD_ROOT/run/user/1000" 2>/dev/null
         fi
-        
+                
         sudo mountpoint -q "$CHARD_ROOT/run/dbus"   || sudo mount --bind /run/dbus "$CHARD_ROOT/run/dbus" 2>/dev/null
         sudo mountpoint -q "$CHARD_ROOT/dev/dri"    || sudo mount --bind /dev/dri "$CHARD_ROOT/dev/dri" 2>/dev/null
         sudo mountpoint -q "$CHARD_ROOT/dev/input"  || sudo mount --bind /dev/input "$CHARD_ROOT/dev/input" 2>/dev/null
-        
+                
         if [ -f "/home/chronos/user/.bashrc" ]; then
             sudo mountpoint -q "$CHARD_ROOT/run/cras" || sudo mount --bind /run/cras "$CHARD_ROOT/run/cras" 2>/dev/null
         else
             sudo mountpoint -q "$CHARD_ROOT/run/cras" || sudo mount --bind /run/user/1000/pulse "$CHARD_ROOT/run/cras" 2>/dev/null
         fi
+        
+        sudo mount --bind "$CHARD_ROOT" "$CHARD_ROOT"
+        sudo mount --make-rslave "$CHARD_ROOT"
         
         sudo chroot "$CHARD_ROOT" /bin/bash -c "
 
@@ -406,21 +409,23 @@ case "$cmd" in
         
         if [ -f "/home/chronos/user/.bashrc" ]; then
             sudo umount -l "$CHARD_ROOT/run/cras" 2>/dev/null || true
-
+        
         else
             sudo umount -l "$CHARD_ROOT/run/cras" 2>/dev/null || true
         fi
-        
+                
         sudo umount -l "$CHARD_ROOT/dev/input"  2>/dev/null || true
         sudo umount -l "$CHARD_ROOT/dev/dri"    2>/dev/null || true
         sudo umount -l "$CHARD_ROOT/run/dbus"   2>/dev/null || true
-        
+                
         if [ -f "/home/chronos/user/.bashrc" ]; then
             sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
             sudo umount -l "$CHARD_ROOT/run/chrome" 2>/dev/null || true
         else
             sudo umount -l "$CHARD_ROOT/run/user/1000" 2>/dev/null || true
         fi
+        
+        sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
         ;;
     categories|cat)
         PORTAGE_DIR="$CHARD_ROOT/usr/portage"
