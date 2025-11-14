e7332e21f83734893fa1652551e544842b319936
shadowed1 <itszachg@gmail.com>
2025-11-13 23:45:16 -0500

    Update chard.sh

--- chard.sh
+++ chard.sh
@@ -282,6 +282,7 @@ case "$cmd" in
             sudo mountpoint -q "$CHARD_ROOT/run/cras" || sudo mount --bind /run/user/1000/pulse "$CHARD_ROOT/run/cras" 2>/dev/null
         fi
         
+        sudo mount --bind "$CHARD_ROOT" "$CHARD_ROOT"
         sudo mount --make-rslave "$CHARD_ROOT"
         
         sudo chroot "$CHARD_ROOT" /bin/bash -c "
