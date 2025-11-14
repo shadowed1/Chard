2445bce723f1eb9d27f58f27b1ff1ba4443b7347
shadowed1 <itszachg@gmail.com>
2025-11-14 16:24:43 -0500

    cleanup?

--- Chard_Installer.sh
+++ Chard_Installer.sh
@@ -1785,4 +1785,20 @@ sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
 show_progress
 echo "${GREEN}[+] Chard Root is ready! Open a new shell and enter chard root with: ${RESET}"
 sudo cp "$CHARD_ROOT/chardbuild.log" ~/
+sudo umount -l "$CHARD_ROOT/run/cras"                           2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/dev/input"                          2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/dev/dri"                            2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/run/chrome"                         2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/run/dbus"                           2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/etc/ssl"                            2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/dev/pts"                            2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/dev/shm"                            2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/dev"                                2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/sys"                                2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/proc"                               2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/tmp/usb_mount"                      2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/run/user/1000"                      2>/dev/null || true
+sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap"               2>/dev/null || true
+sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
 echo "${YELLOW}Copied chardbuild.log to $HOME ${RESET}"
