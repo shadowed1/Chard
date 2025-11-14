f822438166030c7eb6a6926e804ae2c903bc795d
shadowed1 <itszachg@gmail.com>
2025-11-14 16:28:29 -0500

    Update Chard_Installer.sh

--- Arch/Chard_Installer.sh
+++ Arch/Chard_Installer.sh
@@ -158,7 +158,6 @@ cleanup_chroot() {
     sudo umount -l "$CHARD_ROOT/run/chrome"                         2>/dev/null || true
     sudo umount -l "$CHARD_ROOT/run/dbus"                           2>/dev/null || true
     sudo umount -l "$CHARD_ROOT/etc/ssl"                            2>/dev/null || true
-    sudo umount -l "$CHARD_ROOT/etc/"                               2>/dev/null || true
     sudo umount -l "$CHARD_ROOT/dev/pts"                            2>/dev/null || true
     sudo umount -l "$CHARD_ROOT/dev/shm"                            2>/dev/null || true
     sudo umount -l "$CHARD_ROOT/dev"                                2>/dev/null || true
@@ -167,7 +166,6 @@ cleanup_chroot() {
     sudo umount -l "$CHARD_ROOT/tmp/usb_mount"                      2>/dev/null || true
     sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
     sudo umount -l "$CHARD_ROOT/run/user/1000"                      2>/dev/null || true
-    sudo umount -l "$CHARD_ROOT/run"                                2>/dev/null || true
     sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap"               2>/dev/null || true
     sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
     echo "${YELLOW}Copied chardbuild.log to $HOME ${RESET}"
@@ -187,7 +185,6 @@ sudo umount -l "$CHARD_ROOT/dev/dri"                            2>/dev/null || t
 sudo umount -l "$CHARD_ROOT/run/chrome"                         2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/run/dbus"                           2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/etc/ssl"                            2>/dev/null || true
-sudo umount -l "$CHARD_ROOT/etc/"                               2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/dev/pts"                            2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/dev/shm"                            2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/dev"                                2>/dev/null || true
@@ -196,7 +193,6 @@ sudo umount -l "$CHARD_ROOT/proc"                               2>/dev/null || t
 sudo umount -l "$CHARD_ROOT/tmp/usb_mount"                      2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/run/user/1000"                      2>/dev/null || true
-sudo umount -l "$CHARD_ROOT/run"                                2>/dev/null || true
 sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap"               2>/dev/null || true
 sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
 echo "${RED}[*] Removing $CHARD_ROOT...${RESET}"
@@ -1783,6 +1779,23 @@ else
     sudo umount -l "$CHARD_ROOT/run/user/1000" 2>/dev/null || true
 fi
 
+sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
+
+sudo umount -l "$CHARD_ROOT/run/cras"                           2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/dev/input"                          2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/dev/dri"                            2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/run/chrome"                         2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/run/dbus"                           2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/etc/ssl"                            2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/dev/pts"                            2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/dev/shm"                            2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/dev"                                2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/sys"                                2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/proc"                               2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/tmp/usb_mount"                      2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
+sudo umount -l "$CHARD_ROOT/run/user/1000"                      2>/dev/null || true
+sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap"               2>/dev/null || true
 sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
 
 show_progress
