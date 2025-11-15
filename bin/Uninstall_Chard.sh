d673d87e53fe773596808596172b5d60889c88b3
shadowed1 <itszachg@gmail.com>
2025-11-15 02:16:17 -0500

    Update Uninstall_Chard.sh

--- bin/Uninstall_Chard.sh
+++ bin/Uninstall_Chard.sh
@@ -33,6 +33,7 @@ if [[ "$ans" =~ ^[Yy]$ ]]; then
             sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
             sudo umount -l "$CHARD_ROOT/run/user/1000" 2>/dev/null || true
             sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap" 2>/dev/null || true
+            sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
             
             echo "${RED}[*] Removing $CHARD_ROOT...${RESET}"
             sudo rm -rf "$CHARD_ROOT" 2>/dev/null
