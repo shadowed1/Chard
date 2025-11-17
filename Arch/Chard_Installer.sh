b6c743d6ec1c6d5e4c7d70b89abd79ea77d6f691
shadowed1 <itszachg@gmail.com>
2025-11-17 02:32:05 -0500

    Update Chard_Installer.sh

--- Arch/Chard_Installer.sh
+++ Arch/Chard_Installer.sh
@@ -1872,6 +1872,6 @@ sleep 0.2
 sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
 sleep 0.2
 show_progress
-sudo cp /etc/asound.conf "$CHARD_ROOT/etc/asound.conf"
+sudo cp /etc/asound.conf "$CHARD_ROOT/etc/asound.conf" 2>/dev/null
 echo "${GREEN}[+] Chard Root is ready! Restart is recommended. ${RESET}"
 
