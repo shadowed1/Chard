61e3a88c804c4c1dcdac948b212160a4812cb419
shadowed1 <itszachg@gmail.com>
2025-11-17 17:00:06 -0500

    Update chard.sh

--- Arch/chard.sh
+++ Arch/chard.sh
@@ -504,6 +504,41 @@ case "$cmd" in
             exit 1
         fi
         ;;
+    unmount)
+        sudo umount -l "$CHARD_ROOT/run/cras"                           2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/dev/input"                          2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/dev/dri"                            2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/run/chrome"                         2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/run/dbus"                           2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/etc/ssl"                            2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/dev/pts"                            2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/dev/shm"                            2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/dev"                                2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/sys"                                2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/proc"                               2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/tmp/usb_mount"                      2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/run/user/1000"                      2>/dev/null || true
+        sleep 0.2
+        sudo umount -l -f "$CHARD_ROOT/usr/bin/bwrap"                   2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
+        sleep 0.2
+        sudo setfacl -Rb /run/chrome 2>/dev/null
+        ;;
     *)
         chard_run "$cmd" "$@"
         ;;
