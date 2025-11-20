a5b281ffe3f6f2b085e73a99b793917a3b94f5b2
shadowed1 <itszachg@gmail.com>
2025-11-19 23:18:31 -0500

    Update chard.sh

--- Arch/chard.sh
+++ Arch/chard.sh
@@ -540,9 +540,9 @@ case "$cmd" in
         sleep 0.2
         sudo umount -l -f "$CHARD_ROOT/usr/bin/bwrap"                   2>/dev/null || true
         sleep 0.2
-        sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
-        sleep 0.2
         sudo umount -l -f "$CHARD_ROOT/usr/local/bubbepatch/bin/bwrap" 2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
         sleep 0.2
         sudo setfacl -Rb /run/chrome 2>/dev/null
         echo
