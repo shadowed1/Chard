dcc857f6a37ab74f1152b48c4a68a3bcb511464c
shadowed1 <itszachg@gmail.com>
2025-11-20 01:04:14 -0500

    Update Chard_Installer.sh

--- Gentoo/Chard_Installer.sh
+++ Gentoo/Chard_Installer.sh
@@ -166,6 +166,7 @@ cleanup_chroot() {
     sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
     sudo umount -l "$CHARD_ROOT/run/user/1000" 2>/dev/null || true
     sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap" 2>/dev/null || true
+    sudo umount -l -f "$CHARD_ROOT/usr/local/bubblepatch/bin/bwrap" 2>/dev/null || true
     sudo cp "$CHARD_ROOT/chardbuild.log" ~/
     echo "${YELLOW}Copied chardbuild.log to $HOME ${RESET}"
 }
@@ -193,6 +194,7 @@ sudo umount -l "$CHARD_ROOT/tmp/usb_mount" 2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
 sudo umount -l "$CHARD_ROOT/run/user/1000" 2>/dev/null || true
 sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap" 2>/dev/null || true
+sudo umount -l -f "$CHARD_ROOT/usr/local/bubblepatch/bin/bwrap" 2>/dev/null || true
 echo "${RED}[*] Removing $CHARD_ROOT...${RESET}"
 sudo rm -rf "$CHARD_ROOT" 2>/dev/null
 
