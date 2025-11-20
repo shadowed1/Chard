62e755f95484de25335edf4b292d47455e9f3023
shadowed1 <itszachg@gmail.com>
2025-11-19 23:21:10 -0500

    Update Reinstall_Chard.sh

--- Arch/Reinstall_Chard.sh
+++ Arch/Reinstall_Chard.sh
@@ -41,8 +41,13 @@ cleanup_chroot() {
     sleep 0.2
     sudo umount -l -f "$CHARD_ROOT/usr/bin/bwrap" 2>/dev/null || true
     sleep 0.2
+    sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap"               2>/dev/null || true
+    sleep 0.2
+    sudo umount -l -f "$CHARD_ROOT/usr/local/bubbepatch/bin/bwrap" 2>/dev/null || true
+    sleep 0.2
     sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
     sleep 0.2
+    sudo setfacl -Rb /run/chrome 2>/dev/null
 }
 
 trap cleanup_chroot EXIT INT TERM
