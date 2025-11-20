f9f1b9d2aaaa7d8ee9818bef57a2422b2f45e47a
shadowed1 <itszachg@gmail.com>
2025-11-19 23:18:51 -0500

    Update Chard_Installer.sh

--- Arch/Chard_Installer.sh
+++ Arch/Chard_Installer.sh
@@ -181,6 +181,8 @@ cleanup_chroot() {
     sleep 0.2
     sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap"               2>/dev/null || true
     sleep 0.2
+    sudo umount -l -f "$CHARD_ROOT/usr/local/bubbepatch/bin/bwrap" 2>/dev/null || true
+    sleep 0.2
     sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
 }
 
