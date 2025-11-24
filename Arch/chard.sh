655950ec8e5c87d26c34c7765a1453064322a009
shadowed1 <itszachg@gmail.com>
2025-11-24 13:42:17 -0500

    sudo

--- Arch/chard.sh
+++ Arch/chard.sh
@@ -236,14 +236,14 @@ case "$cmd" in
         sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap" 2>/dev/null || true
         sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
         sudo setfacl -Rb /run/chrome 2>/dev/null
-        killall -9 pulseaudio 2>/dev/null
-        pkill -f xfce4-session
-        pkill -f xfwm4
-        pkill -f xfce4-panel
-        pkill -f xfdesktop
-        pkill -f xfce4-terminal
-        pkill -f xfce4-*
-        pkill -f Xorg
+        sudo killall -9 pulseaudio 2>/dev/null
+        sudo pkill -f xfce4-session
+        sudo pkill -f xfwm4
+        sudo pkill -f xfce4-panel
+        sudo pkill -f xfdesktop
+        sudo pkill -f xfce4-terminal
+        sudo pkill -f xfce4-*
+        sudo pkill -f Xorg
         ;;
     chariot)
         sudo mount --bind "$CHARD_ROOT" "$CHARD_ROOT"
