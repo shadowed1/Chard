1368b2b661369f805632ceea6730b4cfa94a0452
shadowed1 <itszachg@gmail.com>
2025-12-01 06:10:16 -0500

    Update Chard_Installer.sh

--- Arch/Chard_Installer.sh
+++ Arch/Chard_Installer.sh
@@ -1894,7 +1894,7 @@ sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
 sleep 0.2
 sudo setfacl -Rb /run/chrome 2>/dev/null
 sleep 0.2
-show_progress
+#show_progress
 CHROMEOS_BASHRC="/home/chronos/user/.bashrc"
 if [ -f "$CHROMEOS_BASHRC" ]; then
     CHROME_MILESTONE=$(grep '^CHROMEOS_RELEASE_CHROME_MILESTONE=' /etc/lsb-release | cut -d'=' -f2)
