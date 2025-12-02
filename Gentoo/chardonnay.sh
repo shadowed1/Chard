b1b06838c81afa1fbe1348c2489e2ee0745e33a6
shadowed1 <itszachg@gmail.com>
2025-12-02 16:18:13 -0500

    Update chardonnay.sh

--- Gentoo/chardonnay.sh
+++ Gentoo/chardonnay.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# sudo ln -s $CHARD_ROOT/usr/bin/xkbcomp /usr/bin/xkbcomp
 SOMMELIER_DISPLAY="/run/chrome/wayland-0"
 SOMMELIER_DRM_DEVICE="/dev/dri/renderD128"
 ARCH="$(uname -m)"
+
 SOMMELIER_CMD=(
     sommelier
     --display="$SOMMELIER_DISPLAY"
@@ -10,20 +10,18 @@ SOMMELIER_CMD=(
     -X
     --glamor
     --enable-linux-dmabuf
-    --xwayland-path=$CHARD_ROOT/usr/libexec/Xwayland)
+    --xwayland-path="$CHARD_ROOT/usr/libexec/Xwayland"
+)
 
 if [[ "$ARCH" == "x86_64" ]]; then
-    SOMMELIER_CMD+=( --force-drm-device="$SOMMELIER_DRM_DEVICE" )
+    SOMMELIER_CMD+=(--force-drm-device="$SOMMELIER_DRM_DEVICE")
 fi
 
-"${SOMMELIER_CMD[@]}" -- bash -c '
-    sleep 0.1
-    export DISPLAY=$(ls $CHARD_ROOT/tmp/.X11-unix | sed "s/^X/:/" | head -n1)
+env PATH="$CHARD_ROOT/usr/bin:$CHARD_ROOT/bin:$CHARD_ROOT/usr/$CHOST/gcc-bin/14:$CHARD_ROOT/usr/local/bin:$PATH" \
+"${SOMMELIER_CMD[@]}" -- bash -c "
+    echo 'Sommelier shell running...'
+    export CHARD_ROOT='$CHARD_ROOT'
+    export LD_LIBRARY_PATH='$PATH:$CHARD_ROOT/usr/lib:$CHARD_ROOT/lib:$CHARD_ROOT/usr/lib/gcc/$CHOST/14:$CHARD_ROOT/usr/$CHOST/gcc-bin/14'
     cd ~/
     exec bash
-'
-sudo setfacl -Rb /root 2>/dev/null
-if [ -f $CHARD_ROOT/tmp/.pulseaudio_pid ]; then
-    kill "$(cat $CHARD_ROOT/tmp/.pulseaudio_pid)" 2>/dev/null
-    rm -f $CHARD_ROOT/tmp/.pulseaudio_pid
-fi
+"
