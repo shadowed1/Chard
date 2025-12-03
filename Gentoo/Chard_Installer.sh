bbf74ad47ffcb5ff0f3f588be35f74b1aec1d601
shadowed1 <itszachg@gmail.com>
2025-12-03 01:57:50 -0500

    Update Chard_Installer.sh

--- Gentoo/Chard_Installer.sh
+++ Gentoo/Chard_Installer.sh
@@ -2085,6 +2085,10 @@ sudo chroot "$CHARD_ROOT" /bin/bash -c '
     USER_ID=1000
 
     sudo -u "$USER" bash -c "
+        cleanup() {
+            echo \"Logging out $USER\"
+        }
+        trap cleanup EXIT INT TERM
         source \$HOME/.bashrc 2>/dev/null
         source \$HOME/.smrt_env.sh 2>/dev/null
         sudo chown -R 1000:1000 $HOME
