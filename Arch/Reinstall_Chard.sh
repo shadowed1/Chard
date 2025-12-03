20511242d43f4e481e7c10b7c1cd329950a30ef5
shadowed1 <itszachg@gmail.com>
2025-12-03 14:40:11 -0500

    Update Reinstall_Chard.sh

--- Arch/Reinstall_Chard.sh
+++ Arch/Reinstall_Chard.sh
@@ -428,8 +428,15 @@ EOF
                         rm -rf ~/.pulse 2>/dev/null
                         rm -rf ~/.cache/pulse 2>/dev/null
                         sudo -E pacman -Syu --noconfirm pipewire-pulse 2>/dev/null
-                        pactl set-default-sink "Virtio Soundcard Sink"
-                        pactl set-default-source "Virtio Soundcard Source"
+                        sudo rm -f /run/chrome/pipewire-0.lock /run/chrome/pipewire-0-manager.lock
+                        sudo rm -f /run/chrome/pulse/native /run/chrome/pulse/*
+                        pipewire 2>/dev/null &
+                        sleep 0.2
+                        wireplumber 2>/dev/null &
+                        sleep 0.2
+                        pipewire-pulse 2>/dev/null &
+                        pactl set-default-sink "Virtio Soundcard Sink" 2>/dev/null
+                        pactl set-default-source "Virtio Soundcard Source" 2>/dev/null
                     "
                 
                     umount -l /dev/zram0   2>/dev/null || true
@@ -444,7 +451,12 @@ EOF
                 '
                 
                 chard_unmount
-
+                killall -9 pipewire 2>/dev/null
+                killall -9 pipewire-pulse 2>/dev/null
+                killall -9 pulseaudio 2>/dev/null
+                killall -9 wireplumber 2>/dev/null
+                sudo rm -f /run/chrome/pipewire-0.lock /run/chrome/pipewire-0-manager.lock
+                sudo rm -f /run/chrome/pulse/native /run/chrome/pulse/*
                 echo "${MAGENTA}[*] Quick Reinstall complete.${RESET}"
                 echo
                 ;;
