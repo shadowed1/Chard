ae4767b81d1bfbad1cb6285b52e86a7e2ba503e3
shadowed1 <itszachg@gmail.com>
2025-12-04 06:17:32 -0500

    Update Chard_Installer.sh

--- Arch/Chard_Installer.sh
+++ Arch/Chard_Installer.sh
@@ -434,6 +434,10 @@ sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/Arch/cha
 sleep 0.2
 sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/Arch/root.sh"             -o "$CHARD_ROOT/bin/root"
 sleep 0.2
+sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/Arch/chard_volume.sh"            -o "$CHARD_ROOT/bin/chard_volume" 2>/dev/null
+sleep 0.2
+sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/Arch/chardwire.sh"            -o "$CHARD_ROOT/bin/chardwire" 2>/dev/null
+sleep 0.2
 
 sudo chmod +x "$CHARD_ROOT/bin/chard"
 sudo chmod +x "$CHARD_ROOT/bin/chariot"
@@ -449,6 +453,8 @@ sudo chmod +x "$CHARD_ROOT/bin/chard_mount"
 sudo chmod +x "$CHARD_ROOT/bin/chard_unmount"
 sudo chmod +x "$CHARD_ROOT/bin/chardsetup"
 sudo chmod +x "$CHARD_ROOT/bin/root"
+sudo chmod +x "$CHARD_ROOT/bin/chard_volume"
+sudo chmod +x "$CHARD_ROOT/bin/chardwire"
 
 
 for file in \
