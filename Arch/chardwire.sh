9f1f34cfc26d389fe90b3daad08fa3bdcd1bc60c
shadowed1 <itszachg@gmail.com>
2025-12-04 07:03:26 -0500

    Update chardwire.sh

--- Arch/chardwire.sh
+++ Arch/chardwire.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VOLUME_FILE="$CHARD_ROOT/$CHARD_HOME/.chard_volume"
+VOLUME_FILE="$HOME/.chard_volume"
 LAST_VOLUME=""
 
 apply_volume() {
@@ -9,11 +9,12 @@ apply_volume() {
         if [[ "$volume" =~ ^[0-9]+$ ]] && [ "$volume" -ge 0 ] && [ "$volume" -le 100 ] && [ "$volume" != "$LAST_VOLUME" ]; then
             LAST_VOLUME="$volume"
             volume_decimal=$(echo "scale=2; $volume / 100" | bc)
-            wpctl set-volume @DEFAULT_AUDIO_SINK@ "$volume_decimal" 2>/dev/null
+            wpctl set-volume @DEFAULT_AUDIO_SINK@ "$volume_decimal"
+            echo "Volume set to: $volume% ($volume_decimal)"
         fi
     fi
 }
 
-tail -n0 -F "$VOLUME_FILE" 2>/dev/null | while read -r line; do
+tail -n0 -F "$VOLUME_FILE" | while read -r line; do
     apply_volume
 done
