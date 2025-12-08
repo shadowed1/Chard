6a18ac80405ee4572ef629e70bcf2fda9a7569fb
shadowed1 <itszachg@gmail.com>
2025-12-08 17:52:40 -0500

    atomize .chard.volume

--- Arch/chard_volume.sh
+++ Arch/chard_volume.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 get_volume() {
     output=$(cras_test_client 2>/dev/null)
     volume=$(echo "$output" | grep "Output Nodes:" -A 20 | grep "yes" | grep "INTERNAL_SPEAKER" | awk '{print $3}')
@@ -8,11 +9,14 @@ get_volume() {
 update_volume() {
     volume=$(get_volume)
     if [ -n "$volume" ]; then
-        echo "$volume" > "$CHARD_ROOT/$CHARD_HOME/.chard_volume"
+        tmp="$CHARD_ROOT/$CHARD_HOME/.chard_volume.tmp"
+        echo "$volume" > "$tmp"
+        mv "$tmp" "$CHARD_ROOT/$CHARD_HOME/.chard_volume"
     fi
 }
 
 update_volume
+
 dbus-monitor --system "type='signal',interface='org.chromium.cras.Control'" 2>/dev/null | \
 while read -r line; do
     if echo "$line" | grep -q "signal"; then
