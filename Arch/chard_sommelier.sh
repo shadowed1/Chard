aecc8bf7c019767f668f96088762401b1fa2a975
shadowed1 <itszachg@gmail.com>
2025-12-08 21:28:30 -0500

    Update chard_sommelier.sh

--- Arch/chard_sommelier.sh
+++ Arch/chard_sommelier.sh
@@ -17,8 +17,20 @@ SOMMELIER_CMD=(
     --xwayland-path=/usr/bin/Xwayland
 )
 
-"${SOMMELIER_CMD[@]}" -- bash -c '
+if [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
+    "${SOMMELIER_CMD[@]}" -- bash -c '
     sleep 0.1
+    export DISPLAY=$(ls /tmp/.X11-unix | sed "s/^X/:/" | head -n1)
+    [ -f ~/.bashrc ] && source ~/.bashrc
+    pipewire &
+    sleep 0.2
+    pulseaudio &
+    cd ~/
+    exec bash
+'
+else
+   "${SOMMELIER_CMD[@]}" -- bash -c '
+   sleep 0.1
     export DISPLAY=$(ls /tmp/.X11-unix | sed "s/^X/:/" | head -n1)
     [ -f ~/.bashrc ] && source ~/.bashrc
     cd ~/
@@ -29,6 +41,12 @@ SOMMELIER_CMD=(
     chardwire 2>/dev/null &
     exec bash
 '
+fi
+
 sudo setfacl -Rb /root 2>/dev/null
 killall -9 chardwire 2>/dev/null
-killall -9 pulseaudio 2>/dev/null
+
+if [ -f /tmp/.pulseaudio_pid ]; then
+    kill "$(cat /tmp/.pulseaudio_pid)" 2>/dev/null
+    rm -f /tmp/.pulseaudio_pid
+fi
