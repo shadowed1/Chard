a423feffcc6e3a7ef8648c9934b9fe9f61522ec9
shadowed1 <itszachg@gmail.com>
2025-12-12 04:45:45 -0500

    Update chard_sommelier.sh

--- Gentoo/chard_sommelier.sh
+++ Gentoo/chard_sommelier.sh
@@ -16,7 +16,7 @@ SOMMELIER_CMD=(
     --windowed-accelerators="Super_L,<Shift><Control>q,<Alt><Control>slash,<Control>n,<Shift><Control>n,<Control>t,<Shift><Control>t,<Shift><Alt>l,<Alt>bracketleft,<Alt>bracketright,<Alt>equal,<Shift><Alt>m,<Shift><Alt>s,<Alt>1,<Alt>2,<Alt>3,<Alt>4,<Alt>5,<Alt>6,<Alt>7,<Alt>8,<Alt>9,<Shift><Alt>n,<Shift><Control>plus,<Shift><Control>minus,<Shift><Control>0,<Alt><Control>period,<Alt><Control>comma,<Shift><Control>XF86Reload,<Control>XF86Forward,<Control>XF86Back,"
     --frame-color="#202020"
     --enable-linux-dmabuf
-    --xwayland-path=/usr/libexec/Xwayland)
+    --xwayland-path=/usr/libexec/Xwayland
 )
 
    "${SOMMELIER_CMD[@]}" -- bash -c '
