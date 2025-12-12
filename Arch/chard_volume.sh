7852c31fa6a004eecffd0c70571ca6eabd159e62
shadowed1 <itszachg@gmail.com>
2025-12-12 03:47:24 -0500

    HDMI and Headphone support

--- Arch/chard_volume.sh
+++ Arch/chard_volume.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
+get_hdmi() {
+    output=$(cras_test_client 2>/dev/null)
+    
+    hdmi_line=$(echo "$output" | grep "Output Nodes:" -A 20 | grep "HDMI" | grep "yes" | head -n 1)
+    
+    if [ -n "$hdmi_line" ]; then
+        hdmi_name=$(echo "$hdmi_line" | sed -n 's/.*HDMI[[:space:]]*[0-9]*\*\?//p')
+        echo "$hdmi_name"
+    fi
+}
+
 get_volume() {
     output=$(cras_test_client 2>/dev/null)
-    volume=$(echo "$output" | grep "Output Nodes:" -A 20 | grep "yes" | grep "INTERNAL_SPEAKER" | awk '{print $3}')
+    volume=$(echo "$output" | grep "Output Nodes:" -A 20 | grep "yes" | grep -E "INTERNAL_SPEAKER|HEADPHONE|HDMI" | awk '{print $3}')
     echo "$volume"
 }
 
 update_volume() {
     volume=$(get_volume)
+    hdmi=$(get_hdmi)
+    
     if [ -n "$volume" ]; then
         tmp="$CHARD_ROOT/$CHARD_HOME/.chard_volume.tmp"
         echo "$volume" > "$tmp"
         mv "$tmp" "$CHARD_ROOT/$CHARD_HOME/.chard_volume"
+    fi
+    
+    if [ -n "$hdmi" ]; then
+        tmp_hdmi="$CHARD_ROOT/$CHARD_HOME/.chard_hdmi.tmp"
+        echo "$hdmi" > "$tmp_hdmi"
+        mv "$tmp_hdmi" "$CHARD_ROOT/$CHARD_HOME/.chard_hdmi"
     fi
 }
 
