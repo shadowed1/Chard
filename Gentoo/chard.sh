f7274203bc4beda228db3664be232731a401d15d
shadowed1 <itszachg@gmail.com>
2025-12-11 22:13:18 -0500

    remove dbus

--- Gentoo/chard.sh
+++ Gentoo/chard.sh
@@ -44,7 +44,7 @@ chard_unmount() {
         sleep 0.2
         sudo umount -l "$CHARD_ROOT/dev/dri"    2>/dev/null || true
         sleep 0.2
-        sudo umount -l "$CHARD_ROOT/run/dbus"   2>/dev/null || true
+        #sudo umount -l "$CHARD_ROOT/run/dbus"   2>/dev/null || true
         sleep 0.2
         sudo umount -l "$CHARD_ROOT/run/chrome" 2>/dev/null || true
         sleep 0.2
