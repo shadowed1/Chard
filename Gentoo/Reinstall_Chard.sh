c4036e9b5b67ed3377e9bbf88fb4d6fa2f0721ed
shadowed1 <itszachg@gmail.com>
2025-12-11 20:57:43 -0500

    Update Reinstall_Chard.sh

--- Gentoo/Reinstall_Chard.sh
+++ Gentoo/Reinstall_Chard.sh
@@ -319,6 +319,8 @@ trap cleanup_chroot EXIT INT TERM
                 sleep 0.2
                 sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/Gentoo/chardonnay.sh"         -o "$CHARD_ROOT/bin/chardonnay"
                 sleep 0.2
+                sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/chard_preload.sh"         -o "$CHARD_ROOT/bin/chard_preload"
+                sleep 0.2
                 
                 sudo chmod +x "$CHARD_ROOT/bin/SMRT"
                 sudo chmod +x "$CHARD_ROOT/bin/chard"
@@ -333,6 +335,7 @@ trap cleanup_chroot EXIT INT TERM
                 sudo chmod +x "$CHARD_ROOT/bin/chard_mount"
                 sudo chmod +x "$CHARD_ROOT/bin/chard_unmount"
                 sudo chmod +x "$CHARD_ROOT/bin/chardonnay"
+                sudo chmod +x "$CHARD_ROOT/bin/chard_preload"
                 
                 for file in \
                 "$CHARD_ROOT/.chardrc" \
