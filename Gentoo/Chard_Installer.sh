2587c22b6f7614cbaef6387783ae0cf5733d38fb
shadowed1 <itszachg@gmail.com>
2025-12-13 17:06:01 -0500

    safety stop

--- Gentoo/Chard_Installer.sh
+++ Gentoo/Chard_Installer.sh
@@ -186,6 +186,41 @@ cleanup_chroot() {
     sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
     sleep 0.2
     sudo setfacl -Rb /run/chrome 2>/dev/null
+    sudo umount -l "$CHARD_ROOT/run/cras"                           2>/dev/null || true
+    sleep 0.2
+    sudo umount -l "$CHARD_ROOT/dev/input"                          2>/dev/null || true
+    sleep 0.2
+    sudo umount -l "$CHARD_ROOT/dev/dri"                            2>/dev/null || true
+    sleep 0.2
+    sudo umount -l "$CHARD_ROOT/run/chrome"                         2>/dev/null || true
+    sleep 0.2
+    sudo umount -l "$CHARD_ROOT/run/dbus"                           2>/dev/null || true
+    sleep 0.2
+    sudo umount -l "$CHARD_ROOT/etc/ssl"                            2>/dev/null || true
+    sleep 0.2
+    sudo umount -l "$CHARD_ROOT/dev/pts"                            2>/dev/null || true
+    sleep 0.2
+    sudo umount -l "$CHARD_ROOT/dev/shm"                            2>/dev/null || true
+    sleep 0.2
+    sudo umount -l "$CHARD_ROOT/dev"                                2>/dev/null || true
+    sleep 0.2
+    sudo umount -l "$CHARD_ROOT/sys"                                2>/dev/null || true
+    sleep 0.2
+    sudo umount -l "$CHARD_ROOT/proc"                               2>/dev/null || true
+    sleep 0.2
+    sudo umount -l "$CHARD_ROOT/tmp/usb_mount"                      2>/dev/null || true
+    sleep 0.2
+    sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
+    sleep 0.2
+    sudo umount -l "$CHARD_ROOT/run/user/1000"                      2>/dev/null || true
+    sleep 0.2
+    sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap"               2>/dev/null || true
+    sleep 0.2
+    sudo umount -l -f "$CHARD_ROOT/usr/local/bubblepatch/bin/bwrap" 2>/dev/null || true
+    sleep 0.2
+    sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
+    sleep 0.2
+    sudo setfacl -Rb /run/chrome 2>/dev/null
 }
 
 existing_int_trap=$(trap -p INT | cut -d"'" -f2)
@@ -271,6 +306,44 @@ sleep 0.2
 sudo rm -rf "$CHARD_ROOT" 2>/dev/null
 
  chard_unmount() {        
+        sudo umount -l "$CHARD_ROOT/run/cras"   2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/dev/input"  2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/dev/dri"    2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/run/dbus"   2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/run/chrome" 2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/etc/ssl"    2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/dev/pts"    2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/dev/shm"    2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/dev"        2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/sys"        2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/proc"       2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/tmp/usb_mount" 2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT/run/user/1000" 2>/dev/null || true
+        sleep 0.2
+        sudo umount -l -f "$CHARD_ROOT/usr/bin/bwrap" 2>/dev/null || true
+        sleep 0.2
+        sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap"               2>/dev/null || true
+        sleep 0.2
+        sudo umount -l -f "$CHARD_ROOT/usr/local/bubblepatch/bin/bwrap" 2>/dev/null || true
+        sleep 0.2
+        sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
+        sleep 0.2
+        sudo setfacl -Rb /run/chrome 2>/dev/null
+        echo
         sudo umount -l "$CHARD_ROOT/run/cras"   2>/dev/null || true
         sleep 0.2
         sudo umount -l "$CHARD_ROOT/dev/input"  2>/dev/null || true
