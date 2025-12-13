af6e9b45bfdc3d1e9697b3608d55b2331f37252c
shadowed1 <itszachg@gmail.com>
2025-12-12 21:34:01 -0500

    Update Reinstall_Chard.sh

--- Arch/Reinstall_Chard.sh
+++ Arch/Reinstall_Chard.sh
@@ -497,6 +497,7 @@ fi
 sudo tee $CHARD_ROOT/etc/pulse/default.pa.d/10-cras.pa > /dev/null << 'EOF'
 load-module module-alsa-sink device=default sink_name=cras_sink
 set-default-sink cras_sink
+load-module module-suspend-on-idle
 EOF
 
 sudo tee $CHARD_ROOT/etc/pulse/default.pa.d/99-disable-hw.pa > /dev/null << 'EOF'
