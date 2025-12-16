c2fc6ace6d934826badd85dc8dd0ee2219a19aec
shadowed1 <itszachg@gmail.com>
2025-12-16 07:43:11 -0500

    Update chard_sommelier.sh

--- Arch/chard_sommelier.sh
+++ Arch/chard_sommelier.sh
@@ -9,18 +9,21 @@ SOMMELIER_CMD=(
     --force-drm-device="$SOMMELIER_DRM_DEVICE"
     --display="$SOMMELIER_DISPLAY"
     --noop-driver
+    --fullscreen-mode=plain 
+    --vm-identifier=termina
+    --direct-scale
     --stable-scaling
     --enable-xshape
     --sd-notify=READY=1
     --allow-xwayland-emulate-screen-pos-size
-    --only-client-can-exit-fullscreen
     --application-id-x11-property=STEAM_GAME
     --accelerators="<Shift><Control>w,<Alt>tab,<Shift><Alt>tab,<Alt>minus," 
     --windowed-accelerators="Super_L,<Shift><Control>q,<Alt><Control>slash,<Control>n,<Shift><Control>n,<Control>t,<Shift><Control>t,<Shift><Alt>l,<Alt>bracketleft,<Alt>bracketright,<Alt>equal,<Shift><Alt>m,<Shift><Alt>s,<Alt>1,<Alt>2,<Alt>3,<Alt>4,<Alt>5,<Alt>6,<Alt>7,<Alt>8,<Alt>9,<Shift><Alt>n,<Shift><Control>plus,<Shift><Control>minus,<Shift><Control>0,<Alt><Control>period,<Alt><Control>comma,<Shift><Control>XF86Reload,<Control>XF86Forward,<Control>XF86Back,"
     --frame-color="#202020"
     --enable-linux-dmabuf
+    --color=auto sommelier
+    --only-client-can-exit-fullscreen
     --xwayland-path=/usr/bin/Xwayland
-    --vm-identifier=termina
 )
 
    "${SOMMELIER_CMD[@]}" -- bash -c '
