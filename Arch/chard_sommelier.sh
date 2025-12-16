31b020b3568367a0feafc2e8446b220c14b71e75
shadowed1 <itszachg@gmail.com>
2025-12-16 09:37:27 -0500

    Update chard_sommelier.sh

--- Arch/chard_sommelier.sh
+++ Arch/chard_sommelier.sh
@@ -14,6 +14,7 @@ SOMMELIER_CMD=(
     --sd-notify=READY=1
     --allow-xwayland-emulate-screen-pos-size
     --only-client-can-exit-fullscreen
+    --application-id-x11-property=STEAM_GAME
     --accelerators="<Shift><Control>w,<Alt>tab,<Shift><Alt>tab,<Alt>minus," 
     --windowed-accelerators="Super_L,<Shift><Control>q,<Alt><Control>slash,<Control>n,<Shift><Control>n,<Control>t,<Shift><Control>t,<Shift><Alt>l,<Alt>bracketleft,<Alt>bracketright,<Alt>equal,<Shift><Alt>m,<Shift><Alt>s,<Alt>1,<Alt>2,<Alt>3,<Alt>4,<Alt>5,<Alt>6,<Alt>7,<Alt>8,<Alt>9,<Shift><Alt>n,<Shift><Control>plus,<Shift><Control>minus,<Shift><Control>0,<Alt><Control>period,<Alt><Control>comma,<Shift><Control>XF86Reload,<Control>XF86Forward,<Control>XF86Back,"
     --frame-color="#202020"
