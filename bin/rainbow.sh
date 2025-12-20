0c601f75939d620aa61f74ed9f73ace4b77f2166
shadowed1 <itszachg@gmail.com>
2025-12-20 13:50:29 -0500

    Update rainbow.sh

--- bin/rainbow.sh
+++ bin/rainbow.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 # ChromeOS Rainbow Shell
+# shadowed1
+# Thanks to days for help with color research and finding device name
+
 RESET=$(tput sgr0)
-export PS1='\u@\h \w \$ '
+
+if [[ -f /tmp/machine-info ]]; then
+    DEVICE_NAME=$(grep '^customization_id=' /tmp/machine-info 2>/dev/null | cut -d= -f2 | tr -d '"')
+else
+    DEVICE_NAME="unknown"
+fi
+
+if [[ -f /sys/class/chromeos/cros_ec/version ]]; then
+    BOARD_REV=$(grep '^Board version:' /sys/class/chromeos/cros_ec/version 2>/dev/null | awk '{print $3}')
+    BOARD_REV="rev$BOARD_REV"
+else
+    BOARD_REV="rev?"
+fi
+
+USER_NAME=${USER:-$(whoami)}
+ORIGINAL_PS1="$USER_NAME@$DEVICE_NAME-$BOARD_REV \w # "
+export ORIGINAL_PS1
 rainbow_indices=()
 
-for ((g=0; g<=5; g++)); do rainbow_indices+=( $((16 + 36*5 + 6*g + 0)) ); done
-for ((r=5; r>=0; r--)); do rainbow_indices+=( $((16 + 36*r + 6*5 + 0)) ); done
-for ((b=0; b<=5; b++)); do rainbow_indices+=( $((16 + 36*0 + 6*5 + b)) ); done
-for ((g=5; g>=0; g--)); do rainbow_indices+=( $((16 + 36*0 + 6*g + 5)) ); done
-for ((r=0; r<=5; r++)); do rainbow_indices+=( $((16 + 36*r + 6*0 + 5)) ); done
-for ((b=5; b>=0; b--)); do rainbow_indices+=( $((16 + 36*5 + 6*0 + b)) ); done
+for ((g=0; g<=5; g++)); do rainbow_indices+=( $((16 + 36*5 + 6*g + 0)) ); done # Red to Yellow Cube
+for ((r=5; r>=0; r--)); do rainbow_indices+=( $((16 + 36*r + 6*5 + 0)) ); done # Yellow to Green Cube
+for ((b=0; b<=5; b++)); do rainbow_indices+=( $((16 + 36*0 + 6*5 + b)) ); done # Green to Cyan Cube
+for ((g=5; g>=0; g--)); do rainbow_indices+=( $((16 + 36*0 + 6*g + 5)) ); done # Cyan to Blue Cube
+for ((r=0; r<=5; r++)); do rainbow_indices+=( $((16 + 36*r + 6*0 + 5)) ); done # Blue to Magenta Cube
+for ((b=5; b>=0; b--)); do rainbow_indices+=( $((16 + 36*5 + 6*0 + b)) ); done # Magenta to Red Cube
 
 num_colors=${#rainbow_indices[@]}
-
 colorize() {
     local i=0
     local buffer=""
@@ -45,4 +63,6 @@ colorize() {
     done
 }
 
-script -qfc "bash -c \"export PS1='\u@\h \w \$ '; exec bash --norc\"" /dev/null | colorize
+script -qfc \
+"export PS1=\"\$ORIGINAL_PS1\"; exec bash --norc -i" \
+/dev/null | colorize
