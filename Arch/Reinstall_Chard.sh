c5ed1e63bc03cd6f01079d170479090de39d9d75
shadowed1 <itszachg@gmail.com>
2025-12-20 12:06:07 -0500

    inotifytools

--- Arch/Reinstall_Chard.sh
+++ Arch/Reinstall_Chard.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+    #!/bin/bash
 # Chard Reinstaller
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
@@ -624,6 +624,7 @@ sudo mkdir -p $CHARD_ROOT/media
                         source \$HOME/.bashrc 2>/dev/null
                         sudo chown -R 1000:1000 \$HOME
                         cd \$HOME
+                        yay -S --noconfirm inotify-tools 2>/dev/null
                         sudo rm /etc/pipewire/pipewire.conf.d/crostini-audio.conf 2>/dev/null
                         sudo mv /usr/share/libalpm/hooks/90-packagekit-refresh.hook /usr/share/libalpm/hooks/90-packagekit-refresh.hook.disabled 2>/dev/null
                         gpg --batch --pinentry-mode loopback --passphrase '' --quick-gen-key \"dummy-kde-wallet\" default default never 2>/dev/null
