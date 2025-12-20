296f200c90b2df73c3ec9035c400135cc201b55d
shadowed1 <itszachg@gmail.com>
2025-12-20 13:28:21 -0500

    Update Reinstall_Chard.sh

--- Arch/Reinstall_Chard.sh
+++ Arch/Reinstall_Chard.sh
@@ -318,6 +318,12 @@ sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/char
 sleep 0.05
 sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/rainbow.sh"            -o "$CHARD_ROOT/bin/rainbow" 2>/dev/null
 sleep 0.05
+sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/rainbow.sh"            -o "$CHARD_ROOT/bin/rainbow" 2>/dev/null
+sleep 0.05
+sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/chard_garcon.sh"            -o "$CHARD_ROOT/bin/chard_garcon" 2>/dev/null
+sleep 0.05
+sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/chard.conf"            -o /etc/init/chard.conf 2>/dev/null
+sleep 0.05
 
 sudo mkdir -p "$CHARD_ROOT/run/udev"
 sudo chmod +x "$CHARD_ROOT/bin/chard"
@@ -339,6 +345,8 @@ sudo chmod +x "$CHARD_ROOT/.chard.preload"
 sudo chmod +x "$CHARD_ROOT/bin/chard_ucm"
 sudo chmod +x "$CHARD_ROOT/bin/chard_preload"
 sudo chmod +x "$CHARD_ROOT/bin/rainbow"
+sudo chmod +x "$CHARD_ROOT/bin/chard_garcon"
+sudo chmod +x /etc/init/chard.conf
 
 for file in \
     "$CHARD_ROOT/.chardrc" \
