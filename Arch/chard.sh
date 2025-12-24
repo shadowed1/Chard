80bba4fa4726a89370be32ff2fb2bf9b72ccc4ec
shadowed1 <itszachg@gmail.com>
2025-12-23 21:50:59 -0500

    Update chard.sh

--- Arch/chard.sh
+++ Arch/chard.sh
@@ -85,8 +85,6 @@ cleanup_chroot() {
     sleep 0.05
     sudo umount -l "$CHARD_ROOT/proc"       2>/dev/null || true
     sleep 0.05
-    sudo umount -l "$CHARD_ROOT/$CHARD_HOME/external" 2>/dev/null || true
-    sleep 0.05
     sudo umount -l "$CHARD_ROOT/tmp/" 2>/dev/null || true
     sleep 0.05
     sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
@@ -207,8 +205,6 @@ chard_unmount() {
     sudo umount -l "$CHARD_ROOT/proc"       2>/dev/null || true
     sleep 0.05
     $CHARD_ROOT/bin/chard_unmount 2>/dev/null
-    sleep 0.05
-    sudo umount -l "$CHARD_ROOT/$CHARD_HOME/external" 2>/dev/null || true
     sleep 0.05
     sudo umount -l "$CHARD_ROOT/tmp/" 2>/dev/null || true
     sleep 0.05
@@ -348,8 +344,6 @@ case "$cmd" in
             sudo mountpoint -q "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" || sudo mount --bind "/home/chronos/user/MyFiles/Downloads" "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null
             sudo mount -o remount,rw,bind "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null
             $CHARD_ROOT/bin/chard_mount 2>/dev/null
-            sudo mountpoint -q "$CHARD_ROOT/$CHARD_HOME/external" || sudo mount --bind /tmp/usb_mount "$CHARD_ROOT/$CHARD_HOME/external" 2>/dev/null
-            sudo mount -o remount,rw,bind "$CHARD_ROOT/$CHARD_HOME/external" 2>/dev/null
         else
             sudo mountpoint -q "$CHARD_ROOT/run/user/1000" || sudo mount --bind /run/user/1000 "$CHARD_ROOT/run/user/1000" 2>/dev/null
         fi
