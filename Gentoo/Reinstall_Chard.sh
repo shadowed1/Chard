262ccda7b16ec68b3a91cab80c1b54cc1a243f4c
shadowed1 <itszachg@gmail.com>
2025-12-23 22:48:56 -0500

    Update Reinstall_Chard.sh

--- Gentoo/Reinstall_Chard.sh
+++ Gentoo/Reinstall_Chard.sh
@@ -35,6 +35,8 @@ cleanup_chroot() {
         sleep 0.05
         sudo umount -l "$CHARD_ROOT/proc"       2>/dev/null || true
         sleep 0.05
+        $CHARD_ROOT/bin/chard_unmount 2>/dev/null
+        sleep 0.05
         sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
         sleep 0.05
         sudo umount -l "$CHARD_ROOT/run/user/1000" 2>/dev/null || true
@@ -156,7 +158,7 @@ trap cleanup_chroot EXIT INT TERM
                     sleep 0.05
                     sudo umount -l "$CHARD_ROOT/proc"       2>/dev/null || true
                     sleep 0.05
-                    $CHARD_ROOT/bin/chard_unmount
+                    $CHARD_ROOT/bin/chard_unmount 2>/dev/null
                     sleep 0.05
                     sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
                     sleep 0.05
