37fa830b036fe716a51b818aedbc3a32e7308a1e
shadowed1 <itszachg@gmail.com>
2025-12-23 22:01:30 -0500

    Update Reinstall_Chard.sh

--- Arch/Reinstall_Chard.sh
+++ Arch/Reinstall_Chard.sh
@@ -264,7 +264,7 @@ detect_gpu_freq() {
         sleep 0.05
         sudo setfacl -Rb /run/chrome 2>/dev/null
         echo
-        echo "${RESET}${YELLOW}Chard safely unmounted${RESET}"
+        echo "${RESET}${GREEN}Chard safely unmounted${RESET}"
         echo
 }
 chard unmount
