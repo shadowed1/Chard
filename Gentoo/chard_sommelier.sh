34fcfbb7a6d6e69bf417f28fb3f21d58086383ca
shadowed1 <itszachg@gmail.com>
2026-01-05 16:50:03 -0500

    Update chard_sommelier.sh

--- Gentoo/chard_sommelier.sh
+++ Gentoo/chard_sommelier.sh
@@ -35,10 +35,11 @@ SOMMELIER_CMD=(
     sleep 0.2
     pulseaudio 2>/dev/null &
     sleep 0.2
+    powercontrol-gui 2>/dev/null &
+    sleep 0.2
     chardwire 2>/dev/null &
     color_reset &
     sleep 0.2
-    powercontrol-gui 2>/dev/null &
     exec bash
 '
 error_color
