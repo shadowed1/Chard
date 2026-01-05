bba145929550637e681d723c44064a0ccd6a7510
shadowed1 <itszachg@gmail.com>
2026-01-05 16:47:39 -0500

    Update chard_sommelier.sh

--- Arch/chard_sommelier.sh
+++ Arch/chard_sommelier.sh
@@ -35,10 +35,11 @@ SOMMELIER_CMD=(
     sleep 0.2
     pulseaudio 2>/dev/null &
     sleep 0.2
+    powercontrol-gui 2>/dev/null &
+    sleep 0.2
     chardwire 2>/dev/null &
     sleep 0.2
     color_reset 2>/dev/null &
-    powercontrol-gui 2>/dev/null &
     exec bash
 '
 error_color
