1c38a7bc6d1786debb0b02f0fafceff6d1e74831
shadowed1 <itszachg@gmail.com>
2026-01-06 15:05:49 -0500

    Update Chard_Installer.sh

--- Gentoo/Chard_Installer.sh
+++ Gentoo/Chard_Installer.sh
@@ -2261,7 +2261,7 @@ sudo mkdir -p $CHARD_ROOT/etc/pulse/default.pa.d/
 sudo mkdir -p $CHARD_ROOT/etc/pulse/
 
 sudo tee $CHARD_ROOT/etc/pulse/default.pa.d/10-cras.pa > /dev/null << 'EOF'
-load-module module-alsa-sink device=default sink_name=cras_sink
+load-module module-alsa-sink device=default sink_name=cras_sink control=none
 set-default-sink cras_sink
 load-module module-suspend-on-idle
 EOF
