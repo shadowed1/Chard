d3fe0b97aca7d360dae0cdd3751a98ec32a976fd
shadowed1 <itszachg@gmail.com>
2026-01-13 23:21:51 -0500

    Update Reinstall_Chard.sh

--- Arch/Reinstall_Chard.sh
+++ Arch/Reinstall_Chard.sh
@@ -560,7 +560,9 @@ ctl.!default {
 }
 EOF
 sudo mkdir -p $CHARD_ROOT/$CHARD_HOME/external 2>/dev/null
+sudo mkdir -p $CHARD_ROOT/$CHARD_HOME/mtp_devices 2>/dev/null
 sudo chown -R chronos:chronos-access $CHARD_ROOT/$CHARD_HOME/external 2>/dev/null
+sudo chown -R chronos:chronos-access $CHARD_ROOT/$CHARD_HOME/mtp_devices 2>/dev/null
 else
 sudo tee "$CHARD_ROOT/etc/asound.conf" 2>/dev/null << 'EOF'
 pcm.!default {
