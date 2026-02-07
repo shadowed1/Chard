712c3bba8835ef84d1ab24bbfd426799d5a422c6
shadowed1 <itszachg@gmail.com>
2026-02-07 08:56:23 -0500

    firefox audio fix

--- Arch/Reinstall_Chard.sh
+++ Arch/Reinstall_Chard.sh
@@ -526,7 +526,7 @@ export ALSOFT_DRIVERS=alsa
 EOF
 sudo chmod +x "$CHARD_ROOT/bin/chard_prismlauncher"
 
-sudo tee "$CHARD_ROOT/bin/chard_firefox" >/dev/null <<'EOF'
+sudo tee /bin/chard_firefox >/dev/null <<'EOF'
 #!/bin/bash
 CHARD_HOME=$(cat /.chard_home)
 CHARD_USER=$(cat /.chard_user)
@@ -534,10 +534,59 @@ HOME=/$CHARD_HOME
 USER=$CHARD_USER
 PATH=/usr/local/bubblepatch/bin:$PATH
 xhost +SI:localuser:$USER
-source ~/.bashrc
-sudo -u $CHARD_USER /usr/bin/firefox
+sudo -u $CHARD_USER \
+  env HOME=/$CHARD_HOME \
+      PULSE_SERVER=unix:/run/chrome/pulse/native \
+      MOZ_CUBEB_FORCE_PULSE=1 \
+      DISPLAY=:0 \
+      WAYLAND_DISPLAY=wayland-0 \
+      XDG_RUNTIME_DIR=/usr/local/chard/run/chrome \
+      DBUS_SESSION_BUS_ADDRESS="$(cat /.chard_dbus | grep DBUS_SESSION_BUS_ADDRESS | cut -d"'" -f2)" \
+  /usr/bin/firefox "$@"
 EOF
-sudo chmod +x "$CHARD_ROOT/bin/chard_firefox"
+sudo chmod +x /bin/chard_firefox
+
+sudo tee /bin/chard_tor >/dev/null <<'EOF'
+#!/bin/bash
+CHARD_HOME=$(cat /.chard_home)
+CHARD_USER=$(cat /.chard_user)
+HOME=/$CHARD_HOME
+USER=$CHARD_USER
+PATH=/usr/local/bubblepatch/bin:$PATH
+xhost +SI:localuser:$USER
+sudo -u $CHARD_USER \
+  env HOME=/$CHARD_HOME \
+      PULSE_SERVER=unix:/run/chrome/pulse/native \
+      MOZ_CUBEB_FORCE_PULSE=1 \
+      DISPLAY=:0 \
+      WAYLAND_DISPLAY=wayland-0 \
+      XDG_RUNTIME_DIR=/usr/local/chard/run/chrome \
+      DBUS_SESSION_BUS_ADDRESS="$(cat /.chard_dbus | grep DBUS_SESSION_BUS_ADDRESS | cut -d"'" -f2)" \
+  /usr/bin/torbrowser-launcher "$@"
+EOF
+
+sudo chmod +x /bin/chard_tor
+
+sudo tee /bin/chard_thunderbird >/dev/null <<'EOF'
+#!/bin/bash
+CHARD_HOME=$(cat /.chard_home)
+CHARD_USER=$(cat /.chard_user)
+HOME=/$CHARD_HOME
+USER=$CHARD_USER
+PATH=/usr/local/bubblepatch/bin:$PATH
+xhost +SI:localuser:$USER
+sudo -u $CHARD_USER \
+  env HOME=/$CHARD_HOME \
+      PULSE_SERVER=unix:/run/chrome/pulse/native \
+      MOZ_CUBEB_FORCE_PULSE=1 \
+      DISPLAY=:0 \
+      WAYLAND_DISPLAY=wayland-0 \
+      XDG_RUNTIME_DIR=/usr/local/chard/run/chrome \
+      DBUS_SESSION_BUS_ADDRESS="$(cat /.chard_dbus | grep DBUS_SESSION_BUS_ADDRESS | cut -d"'" -f2)" \
+  /usr/bin/thunderbird "$@"
+EOF
+
+sudo chmod +x /bin/chard_thunderbird
 
 if [ -f "/home/chronos/user/.bashrc" ]; then
         CHROMEOS_BASHRC="/home/chronos/user/.bashrc"
