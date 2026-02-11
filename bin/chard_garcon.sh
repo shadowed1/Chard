3451f1c7ad489f5cdeccd8dca58b2ceb7105ce3e
shadowed1 <itszachg@gmail.com>
2026-02-11 10:55:28 -0500

    Update chard_garcon.sh

--- bin/chard_garcon.sh
+++ bin/chard_garcon.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 # Version 1.0 
 # chard_garcon 
-# Author: Days
+# Author: Days and shadowed1
+
+# <<< CHARD_ROOT_MARKER >>>
+CHARD_ROOT=""
+CHARD_HOME=""
+CHARD_USER=""
+# <<< END_CHARD_ROOT_MARKER >>>
 
 export PATH="/usr/bin:/bin:/usr/local/bin:/sbin:/usr/sbin:$PATH"
-WATCH_DIR="/home/chronos/user/MyFiles/chard/"
+WATCH_DIR="/$CHARD_HOME/user/MyFiles/chard/"
 
 declare -A APPS=(
     [xfce4-terminal]="sudo -u chronos /bin/bash -c '
@@ -12,18 +18,18 @@ declare -A APPS=(
         [ -f /tmp/chard_dbus_session ] && source /tmp/chard_dbus_session 2>/dev/null
         
         # Environment setup matching auto-launched / upstart xfce4-terminal from chard.sh
-        export HOME=/home/chronos
-        export USER=chronos
+        CHARD_HOME=$(cat /.chard_home)
+        export HOME=$CHARD_HOME
+        CHARD_USER=$(cat /.chard_user)
+        export USER=$CHARD_USER
         export XDG_DATA_DIRS=/usr/local/share:/usr/share
         export XDG_RUNTIME_DIR=/tmp/chard_runtime
         export DISPLAY=:0
         export GDK_BACKEND=x11
         
-        # Source user bashrc for PATH and aliases
-        [ -f /home/chronos/.bashrc ] && source /home/chronos/.bashrc 2>/dev/null
-        
-        # Launch terminal with matching flags
-        xfce4-terminal --disable-server --class=Xfce4-terminal --maximize=true --zoom=1.2
+        [ -f "$HOME/.bashrc" ] && source "$HOME/.bashrc" 2>/dev/null
+        [ -f "$HOME/.smrt_env.sh" ] && source "$HOME/.smrt_env.sh"
+        xfce4-terminal
     '"
     # (TODO) Add: More apps to watch for... and test in the future DX 
 )
