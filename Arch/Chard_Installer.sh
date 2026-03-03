8518b5c65052dedc56141d91afb4856f1ce2ad07
shadowed1 <itszachg@gmail.com>
2026-03-03 11:28:28 -0500

    Update Chard_Installer.sh

--- Arch/Chard_Installer.sh
+++ Arch/Chard_Installer.sh
@@ -1907,6 +1907,33 @@ EOF
 
 sudo chmod +x $CHARD_ROOT/usr/local/bubblepatch/bin/uname
 
+CHRONOS_RC="$CHARD_ROOT/$CHARD_HOME/.${CHARD_USER}rc"
+
+if [[ ! -f "$CHRONOS_RC" ]]; then
+    sudo tee "$CHRONOS_RC" >/dev/null <<'EOF'
+# /etc/skel/.bashrc
+#
+# This file is sourced by all *interactive* bash shells on startup,
+# including some apparently interactive shells such as scp and rcp
+# that can't tolerate any output.  So make sure this doesn't display
+# anything or bad things will happen !
+
+# Test for an interactive shell.  There is no need to set anything
+# past this point for scp and rcp, and it's important to refrain from
+# outputting anything in those cases.
+if [[ $- != *i* ]] ; then
+        # Shell is non-interactive.  Be done now!
+        return
+fi
+
+# Put your fun stuff here.
+EOF
+    sudo chown "$CHARD_USER:$CHARD_USER" "$CHRONOS_RC"
+    echo "${GREEN}Created $CHRONOS_RC - Add your .bashrc entries here! ${RESET}"
+else
+    echo "${CYAN}Skipping $CHRONOS_RC - Already exists."
+fi
+
 CHROMEOS_BASHRC="/home/chronos/user/.bashrc"
 if [ -f "$CHROMEOS_BASHRC" ]; then
     CHROME_MILESTONE=$(grep '^CHROMEOS_RELEASE_CHROME_MILESTONE=' /etc/lsb-release | cut -d'=' -f2)
