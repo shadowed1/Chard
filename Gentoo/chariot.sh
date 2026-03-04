ef72aa76a8ab133372d42d20b7fd38420163cdfa
shadowed1 <itszachg@gmail.com>
2026-03-04 02:18:19 -0500

    Update chariot.sh

--- Gentoo/chariot.sh
+++ Gentoo/chariot.sh
@@ -1114,26 +1114,26 @@ run_checkpoint 119 "sudo -E emerge app-admin/sudo" checkpoint_119
 #run_checkpoint 120 "sudo -E emerge x11-misc/xkeyboard-config" checkpoint_120
 
 
-checkpoint_121() {
+checkpoint_120() {
     USE="udisks" sudo -E emerge xfce-base/thunar
     rm -rf /var/tmp/portage/xfce-base/thunar-*
     eclean-dist -d
 }
-run_checkpoint 121 "sudo -E emerge xfce-base/thunar" checkpoint_121
+run_checkpoint 120 "sudo -E emerge xfce-base/thunar" checkpoint_120
 
-checkpoint_122() {
+checkpoint_121() {
     USE="udisks" sudo -E emerge gnome-base/gvfs
     rm -rf /var/tmp/portage/gnome-base/gvfs-*
     eclean-dist -d
 }
-run_checkpoint 122 "sudo -E emerge gnome-base/gvfs" checkpoint_122
+run_checkpoint 121 "sudo -E emerge gnome-base/gvfs" checkpoint_121
 
-checkpoint_123() {
+checkpoint_122() {
     sudo -E emerge xfce-base/xfce4-meta
     rm -rf /var/tmp/portage/xfce-base/xfce4-meta-*
     eclean-dist -d
 }
-run_checkpoint 123 "sudo -E emerge xfce-base/xfce4-meta" checkpoint_123
+run_checkpoint 122 "sudo -E emerge xfce-base/xfce4-meta" checkpoint_122
 
 checkpoint_123() {
     sudo -E emerge media-libs/libao
