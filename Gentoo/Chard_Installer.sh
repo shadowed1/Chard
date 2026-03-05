a8bfa02616d4b7830fb3aeaab51a9f26713ed2e3
shadowed1 <itszachg@gmail.com>
2026-03-05 00:36:44 -0500

    Update Chard_Installer.sh

--- Gentoo/Chard_Installer.sh
+++ Gentoo/Chard_Installer.sh
@@ -297,6 +297,74 @@ sleep 0.2
 sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
 sleep 0.2
 sudo setfacl -Rb /run/chrome 2>/dev/null
+sudo umount -l "$CHARD_ROOT/run/cras"                           2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/dev/input"                          2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/dev/dri"                            2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/run/chrome"                         2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/run/dbus"                           2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/etc/ssl"                            2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/dev/pts"                            2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/dev/shm"                            2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/dev"                                2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/sys"                                2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/proc"                               2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/run/user/1000"                      2>/dev/null || true
+sleep 0.2
+sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap"               2>/dev/null || true
+sleep 0.2
+sudo umount -l -f "$CHARD_ROOT/usr/local/bubblepatch/bin/bwrap" 2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
+sleep 0.2
+sudo setfacl -Rb /run/chrome 2>/dev/null
+sudo umount -l "$CHARD_ROOT/run/cras"                           2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/dev/input"                          2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/dev/dri"                            2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/run/chrome"                         2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/run/dbus"                           2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/etc/ssl"                            2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/dev/pts"                            2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/dev/shm"                            2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/dev"                                2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/sys"                                2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/proc"                               2>/dev/null || true
+sleep 0.2
+$CHARD_ROOT/bin/chard_unmount 2>/dev/null
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/run/user/1000"                      2>/dev/null || true
+sleep 0.2
+sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap"               2>/dev/null || true
+sleep 0.2
+sudo umount -l -f "$CHARD_ROOT/usr/local/bubblepatch/bin/bwrap" 2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
+sleep 0.2
+sudo setfacl -Rb /run/chrome 2>/dev/null
 echo "${RED}[*] Removing $CHARD_ROOT...${RESET}"
 sleep 0.2
 
