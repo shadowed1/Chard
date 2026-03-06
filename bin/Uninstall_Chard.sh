7617054e7389d179b1925fb4168bbae8e3326f03
shadowed1 <itszachg@gmail.com>
2026-03-06 08:34:19 -0500

    Update Uninstall_Chard.sh

--- bin/Uninstall_Chard.sh
+++ bin/Uninstall_Chard.sh
@@ -204,6 +204,7 @@ if [[ "$ans" =~ ^[Yy]$ ]]; then
         
             if [ -n "$TARGET_FILE" ]; then
                 sed -i '/^# <<< CHARD ENV MARKER <<</,/^# <<< END CHARD ENV MARKER <<</d' "$TARGET_FILE"
+                tr -d '\0' < "$TARGET_FILE" > "${TARGET_FILE}.clean" && mv "${TARGET_FILE}.clean" "$TARGET_FILE"
             else
                 echo "${RED}No .bashrc found! ${RESET}"
             fi
