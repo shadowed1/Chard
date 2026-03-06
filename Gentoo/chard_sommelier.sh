663bf3fa82695785e8c16fb3253c2bd162000034
shadowed1 <itszachg@gmail.com>
2026-03-06 12:26:53 -0500

    Update chard_sommelier.sh

--- Gentoo/chard_sommelier.sh
+++ Gentoo/chard_sommelier.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-SOMMELIER_DISPLAY="/run/chrome/wayland-0"
-SOMMELIER_DRM_DEVICE="/dev/dri/renderD128"
+if [ -d "/run/chrome" ]; then
+    SOMMELIER_DISPLAY="/run/chrome/wayland-0"
+else
+   SOMMELIER_DISPLAY="${XDG_RUNTIME_DIR}/wayland-0"
+fi
+SOMMELIER_DRM_DEVICE="/dev/dri/renderD128"SOMMELIER_DRM_DEVICE="/dev/dri/renderD128"
 ARCH="$(uname -m)"
 SOMMELIER_CMD=(
     sommelier
