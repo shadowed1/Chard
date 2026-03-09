65ad17e85036010438f1998ae2ac3542637bb356
shadowed1 <itszachg@gmail.com>
2026-03-09 12:03:45 -0400

    Update Chard_Installer.sh

--- Gentoo/Chard_Installer.sh
+++ Gentoo/Chard_Installer.sh
@@ -2335,6 +2335,18 @@ fi
 
 sudo cp /etc/asound.conf $CHARD_ROOT/etc 2>/dev/null
 
+if [ -f "$CHROMEOS_BASHRC" ]; then
+    selected_locale=$(grep -ri "Selected '" "$CHROMEOS_SESSION_LOG_DIR" 2>/dev/null | sed "s/.*Selected '\([^']*\)'.*/\1/" | tail -1)
+                    
+    if [[ -z "$selected_locale" ]]; then
+        echo "Could not determine language."
+        return 0
+    fi
+
+    echo "${YELLOW}Detected language: ${BOLD}$selected_locale ${RESET}"
+    echo "$selected_locale" | sudo tee "$CHARD_ROOT/.chard_language" > /dev/null
+fi
+
 CHROMEOS_BASHRC="/home/chronos/user/.bashrc"
 if [ -f "$CHROMEOS_BASHRC" ]; then
     CHROME_MILESTONE=$(grep '^CHROMEOS_RELEASE_CHROME_MILESTONE=' /etc/lsb-release | cut -d'=' -f2)
