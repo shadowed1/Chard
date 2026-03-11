ae0cc98035d34059d7cca4833718242b95bc2ef5
shadowed1 <itszachg@gmail.com>
2026-03-11 14:36:54 -0400

    Update Reinstall_Chard.sh

--- Gentoo/Reinstall_Chard.sh
+++ Gentoo/Reinstall_Chard.sh
@@ -335,6 +335,8 @@ trap cleanup_chroot EXIT INT TERM
                 sleep 0.05
                 sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/virtm.c"            -o "$CHARD_ROOT/tmp/virtm.c" 2>/dev/null
                 sleep 0.05
+                sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/Gentoo/chard_mesa.sh"            -o "$CHARD_ROOT/bin/chard_mesa" 2>/dev/null
+                sleep 0.05
                 sudo chmod +x "$CHARD_ROOT/bin/SMRT"
                 sudo chmod +x "$CHARD_ROOT/bin/chard"
                 sudo chmod +x "$CHARD_ROOT/bin/chariot"
@@ -357,6 +359,7 @@ trap cleanup_chroot EXIT INT TERM
                 sudo chmod +x "$CHARD_ROOT/bin/autoclicker"
                 sudo chmod +x "$CHARD_ROOT/bin/chard_mtp_mount"
                 sudo chmod +x "$CHARD_ROOT/bin/chard_mtp_unmount"
+                sudo chmod +x "$CHARD_ROOT/bin/chard_mesa"
                 
                 for file in \
                 "$CHARD_ROOT/.chardrc" \
