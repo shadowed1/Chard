4b0df5bca3e5db4d26801b19409f1fff5945dec6
shadowed1 <itszachg@gmail.com>
2026-03-15 11:08:02 -0400

    Update chard_bridge_daemon.sh

--- bin/chard_bridge_daemon.sh
+++ bin/chard_bridge_daemon.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # This daemon runs in Crostini - syncs Chard desktop stubs to garcon
+# sudo cp /mnt/chromeos/MyFiles/Downloads/chard_icons/chard_bridge_daemon /bin/
+# sudo chmod +x /bin/chard_bridge_daemon
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 CYAN=$(tput setaf 6)
