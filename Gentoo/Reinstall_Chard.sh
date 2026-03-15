a6ccf9596d1d0c2418105aa7914a891ea59dd9b3
shadowed1 <itszachg@gmail.com>
2026-03-15 17:30:11 -0400

    Update Reinstall_Chard.sh

--- Gentoo/Reinstall_Chard.sh
+++ Gentoo/Reinstall_Chard.sh
@@ -945,8 +945,10 @@ EOF
                 echo
                 source "$CHARD_ROOT/.chardrc"
                 echo
-                echo "${BLUE}${BOLD}To enable icon support for Chard, please run the following: ${RESET}"
-                echo "${CYAN}${BOLD}chard_bridge_daemon ${RESET}${CYAN}in crostini.${RESET}"
+                echo "${BLUE}${BOLD}To enable icon support for Chard, please run the following in Crostini: ${RESET}${BLUE}"
+                echo "sudo cp /mnt/chromeos/MyFiles/Downloads/chard_icons/chard_bridge_daemon /bin/"
+                echo "sudo chmod +x /bin/chard_bridge_daemon"
+                echo "chard_bridge_daemon"
                 echo
                 echo "${MAGENTA}${BOLD}[*] Quick reinstall complete.${RESET}"
                 echo
