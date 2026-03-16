3281576dd0e26d37e3eaf3d63f89f73655970f79
shadowed1 <itszachg@gmail.com>
2026-03-15 21:18:45 -0400

    Update chariot.sh

--- Gentoo/chariot.sh
+++ Gentoo/chariot.sh
@@ -1622,8 +1622,13 @@ sudo ln -sf /usr/share/misc/magic.mgc /usr/share/file/misc/magic.mgc
 run_checkpoint 149 "CRAS Audio Patches" checkpoint_149
 
 checkpoint_150() {
-    sudo -E emerge bindfs
-    eclean-dist -d
+    ARCH="$(uname -m)"
+    if [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
+        echo "Skipping bindfs install on ARM ($ARCH)"
+    else
+        sudo -E emerge bindfs
+        eclean-dist -d
+    fi
 }
 run_checkpoint 150 "Bindfs" checkpoint_150
 
