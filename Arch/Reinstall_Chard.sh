4d9ffae41b5ebd36dd87e3393ab99f2f011fcc1e
shadowed1 <itszachg@gmail.com>
2026-03-19 20:13:54 -0400

    microphone

--- Arch/Reinstall_Chard.sh
+++ Arch/Reinstall_Chard.sh
@@ -715,10 +715,12 @@ ctl.!default {
 EOF
 fi
 echo
-sudo tee $CHARD_ROOT/etc/pulse/default.pa.d/10-cras.pa > /dev/null << 'EOF'
+sudo tee "$CHARD_ROOT/etc/pulse/default.pa.d/10-cras.pa" > /dev/null << 'EOF'
 load-module module-alsa-sink device=default sink_name=cras_sink control=none
 load-module module-softvol-sink sink_name=linear_sink master=cras_sink
 set-default-sink linear_sink
+load-module module-alsa-source device=default source_name=cras-source control=none
+set-default-source cras-source
 load-module module-suspend-on-idle
 EOF
 
