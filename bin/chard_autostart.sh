1b70d35c5c7af23a049d5ec10b2201bc4d572a32
shadowed1 <itszachg@gmail.com>
2026-03-22 10:29:33 -0400

    Update and rename chard-start to chard_autostart.sh

--- bin/chard_autostart.sh
+++ bin/chard_autostart.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 # Not final
-BASHRC="/home/chronos/user/.bashrc"
-if [ ! -f "$BASHRC" ]; then
-    logger -t chard "chard-start: $BASHRC not found"
+CHARD_USER=$(awk -F: '$3 == 1000 {print $1}' /etc/passwd)
+CHARD_USER="${CHARD_USER:-chronos}"
+BASHRC=""
+i=0
+while [ "$i" -lt 60 ]; do
+    if [ -f "/home/chronos/user/.bashrc" ]; then
+        BASHRC="/home/chronos/user/.bashrc"
+        break
+    elif [ -f "/home/$CHARD_USER/.bashrc" ]; then
+        BASHRC="/home/$CHARD_USER/.bashrc"
+        break
+    fi
+    sleep 1
+    i=$((i + 1))
+done
+
+if [ -z "$BASHRC" ]; then
+    logger -t chard "Timed out waiting for .bashrc (tried /home/chronos/user/.bashrc and /home/$CHARD_USER/.bashrc)"
     exit 1
 fi
 
