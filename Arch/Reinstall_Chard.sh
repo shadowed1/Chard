215634ef2b4e313b6a218696ce815b943f9767a6
shadowed1 <itszachg@gmail.com>
2026-03-22 15:40:20 -0400

    Update Reinstall_Chard.sh

--- Arch/Reinstall_Chard.sh
+++ Arch/Reinstall_Chard.sh
@@ -303,29 +303,6 @@ detect_gpu_freq() {
         sudo pkill -f Xorg 2>/dev/null
 		sudo pkill -f powercontrol-gui 2>/dev/null
 		sudo pkill -f gedit 2>/dev/null
-        found=0
-		while IFS= read -r pid; do
-		[[ "$pid" -eq "$$" ]] && continue
-		exe=$(readlink -f "/proc/$pid/exe" 2>/dev/null) || continue
-		cmdline=$(tr '\0' ' ' < "/proc/$pid/cmdline" 2>/dev/null)
-		if [[ "$exe" == "$CHARD_ROOT"* ]] || [[ "$cmdline" == *"$CHARD_ROOT"* ]]; then
-			if [[ "$cmdline" == *"$CHARD_ROOT/bin/chard"* ]]; then
-				echo "  ${GREEN}PID $pid: $cmdline"
-				echo "   (skipping) ${RESET}"
-				continue
-			fi
-			echo "  ${RED}PID $pid: $cmdline"
-			echo "    (exe: $exe) ${RESET}"
-			sudo kill "$pid" && echo "${RED}    Killed${RESET}" || echo "${RED}    Failed to kill${RESET}"
-			found=1
-		fi
-		done < <(ls /proc | grep -E '^[0-9]+$')
-					
-		if [[ "$found" -eq 0 ]]; then
-			echo "${GREEN}No processes found from $CHARD_ROOT ${RESET}"
-			echo
-		fi
-        echo
         echo "${RESET}${GREEN}Chard safely unmounted${RESET}"
         echo
 }
