fabfcf5af396a248f59d28cac4cc465d43d4d869
shadowed1 <itszachg@gmail.com>
2026-03-22 10:47:26 -0400

    Update Chard_Installer.sh

--- Gentoo/Chard_Installer.sh
+++ Gentoo/Chard_Installer.sh
@@ -191,7 +191,6 @@ chard_boot_setup() {
 
     sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/chard.conf" -o "/etc/init/chard.conf"
 	sleep 0.05
-	sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/chard_autostart.sh" -o "$CHARD_ROOT/bin/chard_autostart"
     if [ $? -ne 0 ] || [ ! -s "/etc/init/chard.conf" ]; then
         echo ""
         echo "${RED}Failed to download chard.conf${RESET}"
@@ -200,7 +199,6 @@ chard_boot_setup() {
     fi
 
     sudo chmod 644 /etc/init/chard.conf
-	sudo chmod +x "$CHARD_ROOT/bin/chard_autostart"
     sudo initctl reload-configuration 2>/dev/null
 
     echo ""
@@ -208,7 +206,7 @@ chard_boot_setup() {
     echo ""
     return 0
 }
-
+sudo stop chard 2>/dev/null
 chard_boot_setup
     
 CHARD_ROOT="${CHARD_ROOT%/}"
