3bf6c7b6c41752f2afd9918c9c2f4ff122353a84
shadowed1 <itszachg@gmail.com>
2026-03-23 09:46:11 -0400

    Update Chard_Installer.sh

--- Arch/Chard_Installer.sh
+++ Arch/Chard_Installer.sh
@@ -799,7 +799,8 @@ sudo chmod +x "$CHARD_ROOT/bin/chard_mesa"
 sudo chmod +x "$CHARD_ROOT/bin/chard_sommelier_patch"
 sudo chmod +x "$CHARD_ROOT/bin/chard_sommelier_game"
 sudo chmod +x "$CHARD_ROOT/bin/chard_startup"
-
+sudo chmod +x "$CHARD_ROOT/bin/chard_version"
+sudo chown 1000:1000 "$CHARD_ROOT/bin/chard_version"
 
 for file in \
     "$CHARD_ROOT/.chardrc" \
