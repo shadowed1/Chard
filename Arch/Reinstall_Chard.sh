21207d6ae563c7bd59e026fbe2d8b902e44454d3
shadowed1 <itszachg@gmail.com>
2026-03-22 21:48:20 -0400

    Update Reinstall_Chard.sh

--- Arch/Reinstall_Chard.sh
+++ Arch/Reinstall_Chard.sh
@@ -305,6 +305,50 @@ detect_gpu_freq() {
         sudo pkill -f Xorg 2>/dev/null
 		sudo pkill -f powercontrol-gui 2>/dev/null
 		sudo pkill -f gedit 2>/dev/null
+		sudo umount -l "$CHARD_ROOT/etc/hosts"   2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/etc/resolv.conv"   2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/run/cras"   2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/dev/input"  2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/dev/dri"    2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/run/udev"    2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/run/dbus"   2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/run/chrome" 2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/etc/ssl"    2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/dev/pts"    2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/dev/shm"    2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/dev"        2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/sys"        2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/proc"       2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/tmp/" 2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/$CHARD_HOME/user/MyFiles/Downloads" 2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/run/user/1000" 2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT/run/" 2>/dev/null || true
+        sleep 0.05
+        sudo umount -l -f "$CHARD_ROOT/usr/bin/bwrap" 2>/dev/null || true
+        sleep 0.05
+        sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap"               2>/dev/null || true
+        sleep 0.05
+        sudo umount -l -f "$CHARD_ROOT/usr/local/bubblepatch/bin/bwrap" 2>/dev/null || true
+        sleep 0.05
+        sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
+        sleep 0.05
         echo "${RESET}${GREEN}Chard safely unmounted${RESET}"
         echo
 }
