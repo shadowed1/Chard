7a74c1ad71dad70202d0f0d6ca7fcae513de0773
shadowed1 <itszachg@gmail.com>
2026-03-27 11:22:44 -0400

    Update Reinstall_Chard.sh

--- Arch/Reinstall_Chard.sh
+++ Arch/Reinstall_Chard.sh
@@ -232,8 +232,10 @@ detect_gpu_freq() {
                     sleep 0.2
                     sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/chard_shortcut.sh"            -o "$CHARD_ROOT/bin/chard_shortcut" 2>/dev/null
                     sleep 0.2
+					sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/chard_shortcut_daemon.sh"            -o "$CHARD_ROOT/bin/chard_shortcut_daemon" 2>/dev/null
                     sudo chmod +x "$CHARD_ROOT/bin/chard_launch_daemon"
                     sudo chmod +x "$CHARD_ROOT/bin/chard_shortcut"
+					sudo chmod +x "$CHARD_ROOT/bin/chard_shortcut_daemon"
                     sudo chmod +x "/home/chronos/user/MyFiles/Downloads/chard_icons/chard_bridge_daemon"
                     vmc share termina Downloads/chard_icons 2>/dev/null
                 fi
