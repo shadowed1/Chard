f5ac2a3dacfb11748207452e81283716f9a457f3
shadowed1 <itszachg@gmail.com>
2026-03-28 09:09:12 -0400

    Update Chard_Installer.sh

--- Arch/Chard_Installer.sh
+++ Arch/Chard_Installer.sh
@@ -2273,14 +2273,11 @@ chard_unmount
 #$CHARD_ROOT/bin/chard_preload
 $CHARD_ROOT/bin/chard_shortcut 2>/dev/null
 sudo mv "$CHARD_ROOT/usr/bin/reboot" "$CHARD_ROOT/$CHARD_HOME/.reboot.bak" 2>/dev/null
-echo "${GREEN}[+] Chard Root is ready! To use, open a new shell and run: ${BOLD}chard root${RESET}"
-if [[ "$(uname -m)" == "aarch64" ]]; then
-    echo "${YELLOW}[!] ARM64 Devices might need to reboot ChromeOS before proceeding. ${RESET}"
-fi
+
 if [ -f /home/chronos/user/.bashrc ]; then
-    if vmc list | grep -q '^termina'; then
+    if vmc list | grep -q '^termina.* running'; then
         vmc start termina >/dev/null 2>&1 &
-        sleep 6
+        sleep 3
         vmc share termina Downloads
         vmc launch termina -- bash -c 'cat > /tmp/temp_install.sh' <<'EOF'
 lxc start penguin >/dev/null 2>&1 || true
@@ -2291,12 +2288,28 @@ elif [ -f /mnt/chromeos/MyFiles/Downloads/chard_icons/chard_bridge_daemon ]; the
     sudo cp /mnt/chromeos/MyFiles/Downloads/chard_icons/chard_bridge_daemon /bin/
 fi
 sudo chmod +x /bin/chard_bridge_daemon
-nohup chard_bridge_daemon >/dev/null 2>&1 &
+chard_bridge_daemon &
 "
 EOF
     else
-        echo "${RED}Termina VM not found, skipping ${RESET}"
+        echo "${YELLOW}Termina VM not found/running.${RESET}${GREEN}"
+		echo
+		echo "To enable icons and shortcuts for Chard, start Termina (Crostini), and copy paste: ${BOLD}"
+		echo "if [ -f /mnt/shared/MyFiles/Downloads/chard_icons/chard_bridge_daemon ]; then"
+		echo "    sudo cp /mnt/shared/MyFiles/Downloads/chard_icons/chard_bridge_daemon /bin/"
+		echo "elif [ -f /mnt/chromeos/MyFiles/Downloads/chard_icons/chard_bridge_daemon ]; then"
+		echo "    sudo cp /mnt/chromeos/MyFiles/Downloads/chard_icons/chard_bridge_daemon /bin/"
+		echo "fi"
+		echo "sudo chmod +x /bin/chard_bridge_daemon"
+		echo "chard_bridge_daemon &"
+		echo "chard_bridge_daemon startup"
+		echo "${RESET}"
     fi
 else
-    echo "${RED}.bashrc not found, skipping Termina setup ${RESET}"
+    echo "${YELLOW}ChromeOS .bashrc not found, skipping Termina icon syncing. ${RESET}"
+fi
+echo
+echo "${GREEN}[+] Chard Root is ready! To use, open a new shell and run: ${BOLD}chard root${RESET}"
+if [[ "$(uname -m)" == "aarch64" ]]; then
+    echo "${YELLOW}[!] ARM64 Devices might need to reboot ChromeOS before proceeding. ${RESET}"
 fi
