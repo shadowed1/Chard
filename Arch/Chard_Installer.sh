baa526bc438b6f444fdb0bd60ff2f272cb8a7f98
shadowed1 <itszachg@gmail.com>
2026-03-30 20:39:25 -0400

    Update Chard_Installer.sh

--- Arch/Chard_Installer.sh
+++ Arch/Chard_Installer.sh
@@ -2273,41 +2273,6 @@ chard_unmount
 #$CHARD_ROOT/bin/chard_preload
 $CHARD_ROOT/bin/chard_shortcut 2>/dev/null
 sudo mv "$CHARD_ROOT/usr/bin/reboot" "$CHARD_ROOT/$CHARD_HOME/.reboot.bak" 2>/dev/null
-
-if [ -f /home/chronos/user/.bashrc ]; then
-    if vmc list | grep -q '^termina.* running'; then
-        vmc start termina >/dev/null 2>&1 &
-        sleep 3
-        vmc share termina Downloads
-        vmc launch termina -- bash -c 'cat > /tmp/temp_install.sh' <<'EOF'
-lxc start penguin >/dev/null 2>&1 || true
-lxc exec penguin -- bash -c "
-if [ -f /mnt/shared/MyFiles/Downloads/chard_icons/chard_bridge_daemon ]; then
-    sudo cp /mnt/shared/MyFiles/Downloads/chard_icons/chard_bridge_daemon /bin/
-elif [ -f /mnt/chromeos/MyFiles/Downloads/chard_icons/chard_bridge_daemon ]; then
-    sudo cp /mnt/chromeos/MyFiles/Downloads/chard_icons/chard_bridge_daemon /bin/
-fi
-sudo chmod +x /bin/chard_bridge_daemon
-chard_bridge_daemon &
-"
-EOF
-    else
-        echo "${YELLOW}Termina VM not found/running.${RESET}${GREEN}"
-		echo
-		echo "To enable icons and shortcuts for Chard, start Termina (Crostini), and copy paste in Terminal: ${BOLD}"
-		echo "if [ -f /mnt/shared/MyFiles/Downloads/chard_icons/chard_bridge_daemon ]; then"
-		echo "    sudo cp /mnt/shared/MyFiles/Downloads/chard_icons/chard_bridge_daemon /bin/"
-		echo "elif [ -f /mnt/chromeos/MyFiles/Downloads/chard_icons/chard_bridge_daemon ]; then"
-		echo "    sudo cp /mnt/chromeos/MyFiles/Downloads/chard_icons/chard_bridge_daemon /bin/"
-		echo "fi"
-		echo "sudo chmod +x /bin/chard_bridge_daemon"
-		echo "chard_bridge_daemon startup"
-		echo "chard_bridge_daemon &"
-		echo "${RESET}"
-    fi
-else
-    echo "${YELLOW}ChromeOS .bashrc not found, skipping Termina icon syncing. ${RESET}"
-fi
 echo
 echo "${GREEN}"
 echo "To enable icons and shortcuts for Chard, start Termina (Crostini), and copy paste in Terminal: ${BOLD}"
