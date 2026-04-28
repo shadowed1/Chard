d1b8b3571771addaf141ab6f6203d262e6db0291
shadowed1 <itszachg@gmail.com>
2026-04-27 20:04:52 -0400

    Update Chard_Installer.sh

--- Arch/Chard_Installer.sh
+++ Arch/Chard_Installer.sh
@@ -786,6 +786,8 @@ sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/char
 sleep 0.2
 sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/Arch/chard_gpu_sr_setup.sh"            -o "$CHARD_ROOT/bin/chard_gpu_sr_setup" 2>/dev/null
 sleep 0.2
+sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/downgrade_flatpak_bwrap.sh"            -o "$CHARD_ROOT/bin/downgrade_flatpak_bwrap" 2>/dev/null
+sleep 0.2
 sudo chmod +x "$CHARD_ROOT/bin/chard"
 sudo chmod +x "$CHARD_ROOT/bin/chariot"
 sudo chmod +x "$CHARD_ROOT/bin/.rootrc"
@@ -821,6 +823,7 @@ sudo chmod +x "$CHARD_ROOT/bin/chard_version"
 sudo chmod +x "$CHARD_ROOT/bin/vulkan_tester"
 sudo chmod +x "$CHARD_ROOT/bin/chard_timezone_daemon"
 sudo chmod +x "$CHARD_ROOT/bin/chard_gpu_sr_setup"
+sudo chmod +x "$CHARD_ROOT/downgrade_flatpak_bwrap"
 sudo chown 1000:1000 "$CHARD_ROOT/bin/chard_version"
 
 for file in \
