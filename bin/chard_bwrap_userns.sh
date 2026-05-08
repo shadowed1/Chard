06733176cd4c3e96dd3ddc99c2ba310d21a879e8
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-05-08 14:24:44 -0400

    Update chard_bwrap_userns.sh

--- bin/chard_bwrap_userns.sh
+++ bin/chard_bwrap_userns.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+sudo install -o 1000 -g 1000 -m 0755 /dev/stdin "/bin/bwrap_userns" <<'EOF'
 unset LD_PRELOAD
 exec "/usr/local/bwrap-nosuid/bin/bwrap" \
     --unshare-user \
     --uid 0 \
     --gid 0 \
     "$@"
+EOF
+sudo chmod +x "/bin/bwrap_userns"
