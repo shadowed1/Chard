57aa574e0c00c1333950431b2cf96cd5e43f620c
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-05-22 10:17:48 -0400

    Update Reinstall_Chard.sh

--- Arch/Reinstall_Chard.sh
+++ Arch/Reinstall_Chard.sh
@@ -239,11 +239,15 @@ detect_gpu_freq() {
                     mkdir -p /home/chronos/user/MyFiles/Downloads/chard_icons >/dev/null 2>&1
                     sleep 0.2
                     curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/chard_bridge_daemon.sh"            -o "/home/chronos/user/MyFiles/Downloads/chard_icons/chard_bridge_daemon" 2>/dev/null
+					sleep 0.2
                     sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/chard_launch_daemon.sh"            -o "$CHARD_ROOT/bin/chard_launch_daemon" 2>/dev/null
                     sleep 0.2
                     sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/chard_shortcut.sh"            -o "$CHARD_ROOT/bin/chard_shortcut" 2>/dev/null
                     sleep 0.2
 					sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/chard_shortcut_daemon.sh"            -o "$CHARD_ROOT/bin/chard_shortcut_daemon" 2>/dev/null
+					sleep 0.2
+					curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/chard_bridge_daemon.service"            -o "/home/chronos/user/MyFiles/Downloads/chard_icons/chard_bridge_daemon.service" 2>/dev/null
+					sleep 0.2
                     sudo chmod +x "$CHARD_ROOT/bin/chard_launch_daemon"
                     sudo chmod +x "$CHARD_ROOT/bin/chard_shortcut"
 					sudo chmod +x "$CHARD_ROOT/bin/chard_shortcut_daemon"
