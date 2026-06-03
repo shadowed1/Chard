ea7b3e2a67f6e40aa4072934df92365f120690c6
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-06-02 22:53:35 -0400

    Update Chard_Installer.sh

--- Arch/Chard_Installer.sh
+++ Arch/Chard_Installer.sh
@@ -151,6 +151,8 @@ case "$confirm" in
 esac
 
 echo "$CHARD_ROOT" | sudo tee "$CHARD_ROOT/.install_path" >/dev/null
+echo "$XDG_RUNTIME_DIR" | sudo tee "$CHARD_ROOT/.xdg_runtime_dir" >/dev/null
+
 
 chard_boot_setup() {
     local TEST_FILE="/etc/init/.boot_test"
