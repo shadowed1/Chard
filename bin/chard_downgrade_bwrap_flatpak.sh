b8555aa4a7048ce72b8e24522dee4937b0a0f4f6
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-06-05 12:14:52 -0400

    Update chard_downgrade_bwrap_flatpak.sh

--- bin/chard_downgrade_bwrap_flatpak.sh
+++ bin/chard_downgrade_bwrap_flatpak.sh
@@ -20,6 +20,28 @@ sudo ninja -C build install
 cd ~/
 sudo rm -rf bubblepatch
 
+sudo mkdir -p /usr/local/bwrap-0.11
+wget -c -P /tmp https://github.com/containers/bubblewrap/releases/download/v0.11.1/bubblewrap-0.11.1.tar.xz
+tar -xf /tmp/bubblewrap-0.11.1.tar.xz -C /tmp
+cd /tmp/bubblewrap-0.11.1
+meson setup build -Dprefix=/usr/local/bwrap-0.11
+
+meson setup \
+  -Dprefix=/usr/local/bwrap-0.11 \
+  -Drequire_userns=false \
+  -Dselinux=disabled \
+  -Dtests=false \
+  -Dbash_completion=disabled \
+  -Dzsh_completion=disabled \
+  -Dman=disabled \
+  build
+  
+ninja -C build
+sudo ninja -C build install
+cd
+sudo rm -rf /tmp/bubblewrap-* 2>/dev/null
+
+
 sudo mkdir -p /usr/local/flatpak-1.16.3
 sudo rm -rf /tmp/flatpak-* 2>/dev/null
 wget -c -P /tmp https://github.com/flatpak/flatpak/releases/download/1.16.3/flatpak-1.16.3.tar.xz
