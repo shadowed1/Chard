78e209be529e93e5811bd83981724be7111b7b27
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-06-07 22:50:46 -0400

    remove pacman lock

--- Arch/Reinstall_Chard.sh
+++ Arch/Reinstall_Chard.sh
@@ -715,6 +715,7 @@ sudo cp /usr/lib64/libvulkan_asahi.so $CHARD_ROOT/usr/lib64/libvulkan_asahi.so 2
                         source \$HOME/.bashrc 2>/dev/null
                         #sudo chown -R 1000:1000 \$HOME 2>/dev/null
                         cd \$HOME
+						sudo rm -f /var/lib/pacman/db.lck 2>/dev/null
                         sudo rm /etc/pipewire/pipewire.conf.d/crostini-audio.conf 2>/dev/null
                         sudo mv /usr/share/libalpm/hooks/90-packagekit-refresh.hook /usr/share/libalpm/hooks/90-packagekit-refresh.hook.disabled 2>/dev/null
                         gpg --batch --pinentry-mode loopback --passphrase '' --quick-gen-key \"dummy-kde-wallet\" default default never 2>/dev/null
