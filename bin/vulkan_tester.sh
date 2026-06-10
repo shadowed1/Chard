dedfb1d342c7093e01c704518b7acaca7ee3d6a3
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-06-10 08:03:09 -0400

    Update vulkan_tester.sh

--- bin/vulkan_tester.sh
+++ bin/vulkan_tester.sh
@@ -4,26 +4,33 @@ GREEN=$(tput setaf 2)
 YELLOW=$(tput setaf 3)
 BLUE=$(tput setaf 4)
 MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
 BOLD=$(tput bold)
 RESET=$(tput sgr0)
+
 VULKANINFO64=$(command -v vulkaninfo)
 VULKANINFO32=$(command -v vulkaninfo32 || command -v /usr/bin/vulkaninfo32)
 
 #sudo mkdir -p /usr/share/vulkan/registry/backup 
 #sudo mv /usr/share/vulkan/registry/* /usr/share/vulkan/registry/backup/ 2>/dev/null
-sudo pacman -S vulkan-headers --noconfirm --overwrite '*'
-yay -S --noconfirm lib32-vulkan-tools
-sudo pacman -S --noconfirm vulkan-tools
+sudo pacman -S vulkan-headers --noconfirm --overwrite '*' 2>/dev/null
+yay -S --noconfirm lib32-vulkan-tools 2>/dev/null
+sudo pacman -S --noconfirm vulkan-tools 2>/dev/null
+sudo apt install mesa-utils 2>/dev/null
+sudo apt install vulkan-tools 2>/dev/null
+
+echo
+echo
+echo "${BOLD}${RED}< < < Vulkan > > >${RESET}"
+echo
 
 if [[ -z "$VULKANINFO64" ]]; then
     echo "${RED}64-bit vulkaninfo not found. ${RESET}"
+    sleep 5
     exit 1
 fi
-if [[ -z "$VULKANINFO32" ]]; then
-    echo "${YELLOW}32-bit vulkaninfo not found. ${RESET}"
-fi
+
 for icd in /usr/share/vulkan/icd.d/*.json; do
-    echo "${BLUE}Testing ICD:${RESET} ${MAGENTA}${BOLD}$icd ${RESET}"
     if [[ "$icd" == *i686* ]]; then
         if [[ -z "$VULKANINFO32" ]]; then
             echo "${YELLOW}vulkaninfo32 not found. ${RESET}"
@@ -39,9 +46,42 @@ for icd in /usr/share/vulkan/icd.d/*.json; do
     VK_ICD_FILENAMES="$icd" "$cmd" >/dev/null 2>&1
     ret=$?
     if [[ $ret -eq 0 ]]; then
-        echo "${BOLD}${GREEN}Success! ${arch} ICD is working.${RESET}"
+        echo "${RED}$icd ${BOLD}[ON]${RESET}"
     else
-        echo "${BOLD}${RED}${arch} ICD could not initialize.${RESET}"
+        echo "${YELLOW}$icd ${BOLD}[OFF]${RESET}"
     fi
     echo
 done
+
+sleep 1
+echo
+echo "${RESET}"
+echo "${BOLD}${CYAN}< < < EGL > > >${BOLD}"
+echo
+
+if command -v eglinfo >/dev/null 2>&1; then
+    eglinfo 2>/dev/null | grep -E \
+        "EGL vendor string|EGL driver name|OpenGL renderer string|OpenGL core profile renderer"
+else
+    echo
+    echo "${YELLOW}eglinfo not found.${RESET}"
+    echo
+fi
+
+sleep 1
+echo
+echo
+echo "${RESET}"
+echo "${BOLD}${MAGENTA}< < < GLX > > > ${BOLD}"
+echo
+
+if command -v glxinfo >/dev/null 2>&1; then
+    glxinfo -B 2>/dev/null | grep -E \
+        "OpenGL vendor string|OpenGL renderer string|OpenGL core profile renderer string|OpenGL version string"
+else
+    echo
+    echo "${YELLOW}glxinfo not found.${RESET}"
+    echo
+fi
+
+echo "${RESET}"
