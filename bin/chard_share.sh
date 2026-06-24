07610ea190ec6af6174c1881e81aea553e4bb650
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-06-24 07:55:16 -0400

    Create chard_share.sh

---
+++ bin/chard_share.sh
@@ -0,0 +1,12 @@
+if [ -f /mnt/shared/MyFiles/Downloads/chard_icons/chard_bridge_daemon ]; then
+    sudo cp /mnt/shared/MyFiles/Downloads/chard_icons/chard_bridge_daemon /bin/
+    sudo cp /mnt/shared/MyFiles/Downloads/chard_icons/chard_bridge_daemon.service /etc/systemd/system/
+elif [ -f /mnt/chromeos/MyFiles/Downloads/chard_icons/chard_bridge_daemon ]; then
+    sudo cp /mnt/chromeos/MyFiles/Downloads/chard_icons/chard_bridge_daemon /bin/
+    sudo cp /mnt/chromeos/MyFiles/Downloads/chard_icons/chard_bridge_daemon.service /etc/systemd/system/
+fi
+sudo systemctl daemon-reload
+sudo systemctl enable --now chard_bridge_daemon.service
+sudo chmod +x /bin/chard_bridge_daemon
+chard_bridge_daemon startup
+chard_bridge_daemon &  
