f5f0eedac621bb32259db905960eba38af4fb977
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-07-03 13:08:06 -0400

    Update Reinstall_Chard.sh

--- Gentoo/Reinstall_Chard.sh
+++ Gentoo/Reinstall_Chard.sh
@@ -749,7 +749,16 @@ EOF
                     XDG_RUNTIME_VALUE='export XDG_RUNTIME_DIR="$ROOT/run/user/1000"'
                 fi
 				
+				if [[ -z "$XDG_RUNTIME_DIR" ]]; then
+				    if [[ -d /run/chrome ]]; then
+				        XDG_RUNTIME_DIR="/run/chrome/"
+				    else
+				        XDG_RUNTIME_DIR="/run/user/1000/"
+				    fi
+				fi
+				
 				echo "$XDG_RUNTIME_DIR" | sudo tee "$CHARD_ROOT/.xdg_runtime_dir" >/dev/null
+
 
                 
                 sudo sed -i "/# <<< CHARD_XDG_RUNTIME_DIR >>>/,/# <<< END CHARD_XDG_RUNTIME_DIR >>>/c\
