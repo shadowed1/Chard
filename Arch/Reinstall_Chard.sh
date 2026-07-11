9bb59d07c3f5c282929f4de568842be2210f6285
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-07-11 17:32:17 -0400

    Update Reinstall_Chard.sh

--- Arch/Reinstall_Chard.sh
+++ Arch/Reinstall_Chard.sh
@@ -531,6 +531,7 @@ FILES=(
 	"bin/gpu_test.sh|$CHARD_ROOT/bin/gpu_test|1"
 	"bin/chard_timezone_daemon.sh|$CHARD_ROOT/bin/chard_timezone_daemon|1"
 	"bin/chard_sommelier_patch.sh|$CHARD_ROOT/bin/chard_sommelier_patch|1"
+	"bin/chard_startup.sh|$CHARD_ROOT/bin/chard_startup|1"
 )
 
 for entry in "${FILES[@]}"; do
