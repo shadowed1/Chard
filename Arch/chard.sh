f4aad22dc8e0262ddce65421a99a942a4085a899
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-07-11 03:21:57 -0400

    comment out bad home

--- Arch/chard.sh
+++ Arch/chard.sh
@@ -603,7 +603,7 @@ case "$cmd" in
                 sudo setfacl -Rm u:1000:rwx /root 2>/dev/null
                 [ -f \"\$HOME/.bashrc\" ] && source \"\$HOME/.bashrc\" 2>/dev/null
                 [ -f \"\$HOME/.smrt_env.sh\" ] && source \"\$HOME/.smrt_env.sh\"
-                cd ~/
+                #cd ~/
                 exec chard_sommelier
                 "
             /bin/error_color
