ac5310941f78b3a56a821c782b8f01ca14852232
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-07-13 10:42:53 -0400

    Update chard_wrappers.sh

--- Arch/chard_wrappers.sh
+++ Arch/chard_wrappers.sh
@@ -896,6 +896,19 @@ EOF
 
 sudo chmod +x /bin/chard_7z
 
+sudo tee /bin/chard_garcon >/dev/null <<'EOF'
+#!/bin/bash
+# chard_garcon
+# version 1 by days
+LD="/opt/lib64/ld-linux-x86-64.so.2"
+LIB="/opt/lib64:/opt/usr/lib64"
+VSH="/opt/usr/bin/vsh"
+HASH="$(cat /.chard_hash)"
+GARCON="/usr/local/google/cros-containers/bin/garcon"
+exec $LD --library-path $LIB $VSH --vm_name=termina --target_container=penguin --owner_id=$HASH -- $GARCON --client --url "$@"
+EOF
+sudo chmod +x /bin/chard_garcon
+
 sudo tee /usr/share/applications/chard-garcon.desktop > /dev/null << 'EOF'
 [Desktop Entry]
 Name=ChromeOS Host Browser
