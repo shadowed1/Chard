c7f4cd54f0ea19e1a0af7c133bcc1d415aca601a
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-07-14 10:36:43 -0400

    Update chard_browser.sh

--- bin/chard_browser.sh
+++ bin/chard_browser.sh
@@ -7,6 +7,9 @@ MAGENTA=$'\033[35m'
 CYAN=$'\033[36m'
 BOLD=$'\033[1m'
 RESET=$'\033[0m'
+
+CHARD_BROWSER_FILE="/.chard_browser"
+
 echo "${BOLD}${CYAN}"
 echo "Select your default web browser for Chard!"
 echo "${RESET}"
@@ -29,6 +32,7 @@ elif [ -f "/usr/share/applications/org.mozilla.firefox.desktop" ]; then
 else
     BROWSERS+=("chard-garcon.desktop|ChromeOS Chrome")
 fi
+
 add_browser chard-garcon.desktop "ChromeOS' Chrome (Requires Crostini to be running to send command)"
 add_browser org.mozilla.firefox.desktop "Firefox"
 add_browser brave-browser.desktop "Brave"
@@ -49,17 +53,40 @@ add_browser floorp.desktop "Floorp"
 add_browser zen.desktop "Zen Browser"
 add_browser thorium-browser.desktop "Thorium"
 
+SAVED_BROWSER=""
+if [ -f "$CHARD_BROWSER_FILE" ]; then
+    SAVED_BROWSER=$(cat "$CHARD_BROWSER_FILE")
+fi
+
+DEFAULT_INDEX=0
+
+if [ -n "$SAVED_BROWSER" ]; then
+    for i in "${!BROWSERS[@]}"; do
+        IFS='|' read -r desktop name <<< "${BROWSERS[$i]}"
+        if [ "$desktop" = "$SAVED_BROWSER" ]; then
+            DEFAULT_INDEX=$i
+            break
+        fi
+    done
+fi
+
 for i in "${!BROWSERS[@]}"; do
     IFS='|' read -r desktop name <<< "${BROWSERS[$i]}"
+    if [ "$i" -eq "$DEFAULT_INDEX" ]; then
+        printf " ${GREEN}%2d) %s ${RESET}${BOLD}${BLUE}(Current Default)${RESET}\n" "$((i+1))" "${CYAN}$name${RESET}"
+    else
+        printf "${RESET}${GREEN} %2d) %s\n" "$((i+1))" "${CYAN}$name${RESET}"
+    fi
 done
 
-printf " ${GREEN}%2d) Enter a desktop file manually (include the ${BOLD}.desktop${RESET}${GREEN} in the name)\n ${RESET}" $(( ${#BROWSERS[@]} + 1 ))
+printf " ${GREEN}%2d) ${RESET}${CYAN}Enter a desktop file manually (include the ${BOLD}.desktop${RESET}${CYAN} in the name)\n ${RESET}" $(( ${#BROWSERS[@]} + 1 ))
 echo
 
-DEFAULT_BROWSER="${BROWSERS[0]}"
+DEFAULT_BROWSER="${BROWSERS[$DEFAULT_INDEX]}"
 
 while true; do
-    read -t 15 -rp "${CYAN}Default Browser: [1-$(( ${#BROWSERS[@]} + 1 ))] (Defaulting to option 1 in 15 seconds...): ${RESET}" choice
+    read -t 15 -rp "${GREEN}Default Browser: [1-$(( ${#BROWSERS[@]} + 1 ))] (Defaulting to current in 15 seconds...): ${RESET}" choice
+
     if [ $? -ne 0 ] || [ -z "$choice" ]; then
         IFS='|' read -r BROWSER_DESKTOP _ <<< "$DEFAULT_BROWSER"
         echo
@@ -79,10 +106,13 @@ while true; do
     echo "${RED}Invalid selection. ${RESET}"
 done
 
+echo "$BROWSER_DESKTOP" | sudo tee "$CHARD_BROWSER_FILE" > /dev/null
+
 xdg-mime default "$BROWSER_DESKTOP" text/html
 xdg-mime default "$BROWSER_DESKTOP" x-scheme-handler/http
 xdg-mime default "$BROWSER_DESKTOP" x-scheme-handler/https
 xdg-settings set default-web-browser "$BROWSER_DESKTOP"
+
 echo
 echo "${BOLD}${BLUE}Default Browser set to: ${RESET}${MAGENTA}${BOLD}${BROWSER_DESKTOP}${RESET}"
 echo
