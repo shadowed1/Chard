53935c18162e6800524c26d999488382615f6f07
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-07-14 07:05:06 -0400

    chard_garcon

--- Arch/chard_wrappers.sh
+++ Arch/chard_wrappers.sh
@@ -899,12 +899,12 @@ sudo chmod +x /bin/chard_7z
 sudo tee /bin/chard_garcon >/dev/null <<'EOF'
 #!/bin/bash
 # chard_garcon
-# version 2 by days
-LD="/opt/lib64/ld-linux-x86-64.so.2"
-LIB="/opt/lib64:/opt/usr/lib64"
-VSH="/opt/usr/bin/vsh"
-HASH="$(cat /.chard_hash)"
-sudo -n nsenter -t 1 -m -- vsh --vm_name=termina --target_container=penguin --owner_id="$HASH" -- /opt/google/cros-containers/bin/garcon --client --url "$@"
+# by days (iamday) 
+URL="$1"
+if [[ "$URL" != http://* && "$URL" != https://* ]]; then
+    URL="https://$URL"
+fi
+sudo -n nsenter -t 1 -m -- vsh --vm_name=termina --target_container=penguin --owner_id="$(cat /.chard_hash)" -- /opt/google/cros-containers/bin/garcon --client --url "$URL"
 EOF
 sudo chmod +x /bin/chard_garcon
 
