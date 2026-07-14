f5bfe471cc7cf6dc4e613f46ddb321c0a31a31d4
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-07-14 06:49:18 -0400

    Update chard_wrappers.sh

--- Arch/chard_wrappers.sh
+++ Arch/chard_wrappers.sh
@@ -899,13 +899,12 @@ sudo chmod +x /bin/chard_7z
 sudo tee /bin/chard_garcon >/dev/null <<'EOF'
 #!/bin/bash
 # chard_garcon
-# version 1 by days
+# version 2 by days
 LD="/opt/lib64/ld-linux-x86-64.so.2"
 LIB="/opt/lib64:/opt/usr/lib64"
 VSH="/opt/usr/bin/vsh"
 HASH="$(cat /.chard_hash)"
-GARCON="/usr/local/google/cros-containers/bin/garcon"
-exec $LD --library-path $LIB $VSH --vm_name=termina --target_container=penguin --owner_id=$HASH -- $GARCON --client --url "$@"
+sudo -n nsenter -t 1 -m -- vsh --vm_name=termina --target_container=penguin --owner_id="$HASH" -- /opt/google/cros-containers/bin/garcon --client --url "$@"
 EOF
 sudo chmod +x /bin/chard_garcon
 
