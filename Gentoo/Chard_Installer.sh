3bf28d52fe359ed2fb6615a0e1137c04c65ec013
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-07-15 14:43:19 -0400

    Update Chard_Installer.sh

--- Gentoo/Chard_Installer.sh
+++ Gentoo/Chard_Installer.sh
@@ -388,11 +388,11 @@ cleanup_chroot() {
     sleep 0.2
     sudo umount -l -f "$CHARD_ROOT/usr/local/bubblepatch/bin/bwrap" 2>/dev/null || true
     sleep 0.2
-	sudo umount -l "$CHARD_ROOT/opt/lib64"
+	sudo umount -l "$CHARD_ROOT/opt/lib64" 2>/dev/null || true
 	sleep 0.2
-	sudo umount -l "$CHARD_ROOT/opt/usr/lib64"
+	sudo umount -l "$CHARD_ROOT/opt/usr/lib64" 2>/dev/null || true
 	sleep 0.2
-	sudo umount -l "$CHARD_ROOT/opt/usr/bin"
+	sudo umount -l "$CHARD_ROOT/opt/usr/bin" 2>/dev/null || true
 	sleep 0.2
     sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
     sleep 0.2
@@ -506,6 +506,12 @@ sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap"               2>/dev/null || t
 sleep 0.2
 sudo umount -l -f "$CHARD_ROOT/usr/local/bubblepatch/bin/bwrap" 2>/dev/null || true
 sleep 0.2
+sudo umount -l "$CHARD_ROOT/opt/lib64" 2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/opt/usr/lib64" 2>/dev/null || true
+sleep 0.2
+sudo umount -l "$CHARD_ROOT/opt/usr/bin" 2>/dev/null || true
+sleep 0.2
 sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
 sleep 0.2
 sudo setfacl -Rb /run/chrome 2>/dev/null
@@ -541,11 +547,11 @@ sudo umount -l -f "$CHARD_ROOT/$CHARD_HOME/bwrap"               2>/dev/null || t
 sleep 0.2
 sudo umount -l -f "$CHARD_ROOT/usr/local/bubblepatch/bin/bwrap" 2>/dev/null || true
 sleep 0.2
-sudo umount -l "$CHARD_ROOT/opt/lib64"
+sudo umount -l "$CHARD_ROOT/opt/lib64" 2>/dev/null || true
 sleep 0.2
-sudo umount -l "$CHARD_ROOT/opt/usr/lib64"
+sudo umount -l "$CHARD_ROOT/opt/usr/lib64" 2>/dev/null || true
 sleep 0.2
-sudo umount -l "$CHARD_ROOT/opt/usr/bin"
+sudo umount -l "$CHARD_ROOT/opt/usr/bin" 2>/dev/null || true
 sleep 0.2
 sudo umount -l "$CHARD_ROOT" 2>/dev/null || true
 sleep 0.2
